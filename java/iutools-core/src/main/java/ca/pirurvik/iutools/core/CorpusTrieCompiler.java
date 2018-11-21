79e9924e8d12e55b103a577234aecc86ee0c5e53
benoitfarley <benoit.farley@gmail.com>
2018-11-21 12:32:45 -0500

    Changed HansardTrie to CorpusTrieCompiler. Moved Hansard2002 to resources/corpora...

--- java/iutools-core/src/main/java/ca/pirurvik/iutools/core/CorpusTrieCompiler.java
+++ java/iutools-core/src/main/java/ca/pirurvik/iutools/core/CorpusTrieCompiler.java
@@ -2,15 +2,10 @@ package ca.pirurvik.iutools.core;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.net.URL;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Vector;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,13 +13,14 @@ import ca.nrc.datastructure.trie.StringSegmenter;
 import ca.nrc.datastructure.trie.StringSegmenter_IUMorpheme;
 import ca.nrc.datastructure.trie.Trie;
 
+
 /**
  * This creates a Trie of the (Inuktitut) words in the Nunavut Hansard
  *
  */
-public class HansardTrie 
+public class CorpusTrieCompiler 
 {
-	private static Trie hansardTrie;
+	private static Trie corpusTrie;
 	private static HashMap<String,Long> index = new HashMap<String, Long>();
 	private static long maxFreq = 0;
 	private static String entryWithMaxFreq;
@@ -35,18 +31,25 @@ public class HansardTrie
 	public static void main(String[] args) {
 		String dirName = args[0];
 		StringSegmenter morphemeSegmenter = new StringSegmenter_IUMorpheme();
-		hansardTrie = new Trie(morphemeSegmenter);
+		corpusTrie = new Trie(morphemeSegmenter);
 		try {
-			File hansardDirectory = openDirectory("data",dirName);
-			process(hansardDirectory);
+			File corpusDirectory = new File(dirName);
+			process(corpusDirectory);
 			printIndex();
+			printTrie(corpusTrie);
 		} catch (Exception e1) {
 			e1.printStackTrace();
 			System.exit(1);
 		}
 	}
     
-    private static void printIndex() {
+    private static void printTrie(Trie trie) {
+    	System.out.println("The trie looks like this: ");
+    	System.out.println(ca.nrc.json.PrettyPrinter.print(trie));
+	}
+
+	private static void printIndex() {
+		System.out.println("\nIndexed words:");
 		for (Map.Entry<String, Long> entry : index.entrySet()) {
 		    String key = entry.getKey();
 		    long value = entry.getValue().longValue();
@@ -54,7 +57,7 @@ public class HansardTrie
 		}
 		System.out.println("Nb of individual words: "+index.entrySet().size());
 	    System.out.println("Entry with maximum frequency: "+entryWithMaxFreq+" ("+maxFreq+")");
-	    System.out.println("Nb. of words in the trie: "+hansardTrie.getSize());
+	    System.out.println("Nb. of words in the trie: "+corpusTrie.getSize());
 	}
 
 	private static void process(File hansardDirectory) {
@@ -72,19 +75,6 @@ public class HansardTrie
     	}
 	}
 
-	private static File openDirectory(String pirurvikDataDir, String dirName) throws Exception {
-        ClassLoader classLoader = HansardTrie.class.getClassLoader();
-        String packagePath = "ca/pirurvik";
-        String dataPath = packagePath+"/"+pirurvikDataDir;
-        String fullPath = dataPath+"/"+dirName;
-        String filePathFull = null;
-    	URL res = classLoader.getResource(fullPath);
-    	if (res==null)
-    		throw new Exception("File "+dirName+" cannot be found in "+dataPath+".");
-    	filePathFull = res.getPath();
-    	File directory = new File(filePathFull);
-    	return directory;
-	}
 
 	/*private static BufferedReader openFile(String pirurvikDataDir,String fileName) throws Exception {
         ClassLoader classLoader = HansardTrie.class.getClassLoader();
@@ -106,14 +96,15 @@ public class HansardTrie
 	private static void processFile(BufferedReader br) {
 		try {
 			String line;
-			while ((line = br.readLine()) != null) {
+			int limit = 20;
+			while ((line = br.readLine()) != null && limit--!=0) {
 				//System.out.println(line);
 				String[] words = extractWordsFromLine(line);
 				for (int n = 0; n < words.length; n++) {
 					if (isInuktitutWord(words[n])) {
 						System.out.print(words[n]+"...");
 						addToIndex(words[n]);
-						hansardTrie.add(words[n]);
+						corpusTrie.add(words[n]);
 						System.out.print("\n");
 					}
 				}
