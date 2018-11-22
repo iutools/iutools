300e3bcbe4f0f4b0c4392ed2eab9b2154f4440d7
benoitfarley <benoit.farley@gmail.com>
2018-11-22 18:19:40 -0500

    New class used in the process of json-serializing a trie and retrieving a trie from a json.

---
+++ java/iutools-core/src/main/java/ca/nrc/datastructure/trie/TrieWithSegmenterClassname.java
@@ -0,0 +1,31 @@
+package ca.nrc.datastructure.trie;
+
+/* blah */
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Set;
+
+
+public class TrieWithSegmenterClassname {
+
+    private long size;
+    private String segmenterclassname;
+    private TrieNode root;
+    
+    
+    public TrieWithSegmenterClassname(String _segmenterclassname, TrieNode _root, long _size) {
+    	this.size = _size;
+    	this.root = _root;
+    	this.segmenterclassname = _segmenterclassname;
+	}
+    
+    public Trie toTrie() throws Exception {
+    	Class<?> clazz = Class.forName(segmenterclassname);
+    	Constructor<?> ctor = clazz.getConstructor();
+    	StringSegmenter segmenter = (StringSegmenter) ctor.newInstance(new Object[] { });
+    	return new Trie(segmenter,root,size);
+    }
+
+}
