7d9ae919c77347ba3ebce5b0c479d8ff535aeda2
benoitfarley <benoit.farley@gmail.com>
2018-11-22 18:04:59 -0500

    Expérimentation pour trouver la racine depuis le début du mot à l'aide d'un trie de racines.

--- java/iutools-core/src/main/java/ca/inuktitutcomputing/morph/exp/Exp.java
+++ java/iutools-core/src/main/java/ca/inuktitutcomputing/morph/exp/Exp.java
@@ -11,6 +11,7 @@ import java.util.Vector;
 
 import ca.nrc.datastructure.trie.StringSegmenter_Char;
 import ca.nrc.datastructure.trie.Trie;
+import ca.nrc.datastructure.trie.TrieException;
 import ca.nrc.datastructure.trie.TrieNode;
 
 public class Exp {
@@ -19,15 +20,29 @@ public class Exp {
 	private static Trie suff_trie;
 
 	public static void main(String[] args) {
+		String[] roots = new String[]{
+				"umiaq/1n"
+		};
+		String[] suffixes = new String[]{
+				"liuq/1nv", "ti/1vn", "u/1nv",
+				"juq/tv-ger-3s", "juq/1vn", "mik/tn-acc-s"
+		};
 		root_trie = new Trie(new StringSegmenter_Char());
-		root_trie.add("umiaq/1n");
+		for (int i=0; i<roots.length; i++) {
+			try {
+				root_trie.add(roots[i]);
+			} catch (TrieException e) {
+				// TODO Auto-generated catch block
+			}
+		}
 		suff_trie = new Trie(new StringSegmenter_Char());
-		suff_trie.add("liuq/1nv");
-		suff_trie.add("ti/1vn");
-		suff_trie.add("u/1nv");
-		suff_trie.add("juq/tv-ger-3s");
-		suff_trie.add("juq/1vn");
-		suff_trie.add("mik/tn-acc-s");
+		for (int i=0; i<suffixes.length; i++) {
+			try {
+				suff_trie.add(suffixes[i]);
+			} catch (TrieException e) {
+				// TODO Auto-generated catch block
+			}
+		}
 
 		System.out.print("Enter a word: ");
 		BufferedReader reader = new BufferedReader(new InputStreamReader(
@@ -53,11 +68,12 @@ public class Exp {
 		TrieNode[] rootsForIncompleteSurfaceForm = new TrieNode[]{};
 		String currentKey = null;
 
+		int charCounter;
 		// Parse the root
-		for (int i = 0; i < chars.length; i++) {
+		for (charCounter = 0; charCounter < chars.length; charCounter++) {
 			previousKeys = (Vector<String>) currentKeys.clone();
-			currentKey = chars[i];
-			currentKeys.add(chars[i]);
+			currentKey = chars[charCounter];
+			currentKeys.add(chars[charCounter]);
 			currentRootNode = root_trie.getNode(currentKeys
 					.toArray(new String[] {}));
 			if (currentRootNode == null) {
@@ -74,10 +90,11 @@ public class Exp {
 				Vector<String> searchKeys = (Vector<String>) currentKeys
 						.clone();
 				searchKeys.add("/");
+				System.out.println("Search for "+Arrays.toString(searchKeys.toArray(new String[] {})));
 				TrieNode slashNode = root_trie.getNode(searchKeys
 						.toArray(new String[] {}));
 				if (slashNode != null) {
-//					rootsForCompleteSurfaceForm = slashNode.getAllTerminals();
+					rootsForCompleteSurfaceForm = slashNode.getAllTerminals();
 					eatenKeys = (Vector<String>) currentKeys.clone();
 					break;
 				}
@@ -85,6 +102,15 @@ public class Exp {
 			previousNode = currentRootNode;
 			eatenKeys = (Vector<String>) currentKeys.clone();
 		}
+		
+		/*
+		 *  Arrivés ici, on a trouvé un chemin dans le trie des racines avec un maximum de lettres du mot
+		 *  initial, mais ce n'est pas une racine complète.
+		 *  Comme il est possible qu'une consonne finale ait été supprimée, on va avancer d'un nœud et vérifier
+		 *  si cela nous amène à une racine complète.
+		 */
+		System.out.println("root: "+root.getText());
+		
 		if (rootsForCompleteSurfaceForm.length==0) {
 			if (root != null) {
 				HashMap<String, TrieNode> children = root.getChildren();
@@ -92,26 +118,34 @@ public class Exp {
 						new String[] {});
 				for (int i = 0; i < childrenKeys.length; i++) {
 					TrieNode nextNode = children.get(childrenKeys[i]);
+					System.out.println("nextNode: "+nextNode.getText());
 					HashMap<String, TrieNode> nextChildren = nextNode
 							.getChildren();
 					String[] nextChildrenKeys = nextChildren.keySet().toArray(
 							new String[] {});
 					for (int j = 0; j < nextChildrenKeys.length; j++) {
 						if (nextChildrenKeys[j].equals("/")) {
-							Vector<String> slashKeys = new Vector<String>();
-							slashKeys.addAll(previousKeys);
-							slashKeys.add(childrenKeys[i]);
-							slashKeys.add("/");
-							String[] keys = slashKeys
-									.toArray(new String[] {});
-							TrieNode slashNode = root_trie.getNode(keys);
-							TrieNode[] possibleRoots = new TrieNode[]{}; //slashNode.getAllTerminals();
+							System.out.println("/");
+							
+//							Vector<String> slashKeys = new Vector<String>();
+//							slashKeys.addAll(previousKeys);
+//							slashKeys.add(childrenKeys[i]);
+//							slashKeys.add("/");
+//							String[] keys = slashKeys
+//									.toArray(new String[] {});
+//							TrieNode slashNode = root_trie.getNode(keys);
+							TrieNode slashNode = nextChildren.get(nextChildrenKeys[j]);
+							TrieNode[] possibleRoots = slashNode.getAllTerminals();
+							for (int k=0; k<possibleRoots.length; k++) {
+								System.out.println("possible root: "+possibleRoots[k].getText());
+							}
+							System.out.println("childrenKeys[i]: "+childrenKeys[i]);
 							if (childrenKeys[i].equals("q")) {
 								if (currentKey.equals("r")) {
 									eatenKeys.add(currentKey);
-									rootsForIncompleteSurfaceForm = possibleRoots
-											.clone();
 								}
+								rootsForIncompleteSurfaceForm = possibleRoots
+											.clone();
 							}
 						}
 					}
@@ -129,6 +163,9 @@ public class Exp {
 				System.out.println("Possible root (incomplete surface form): "
 						+ rootsForIncompleteSurfaceForm[i].getText());
 			}
+			
+			
+			System.out.println("\nAnalyser le reste du mot à partir du caractère "+charCounter+" : "+string.substring(charCounter));
 		}
 	}
 }
