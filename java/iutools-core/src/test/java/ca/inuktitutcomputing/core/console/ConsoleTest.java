3912479116025d1a55211e8ec3969d707a098379
benoitfarley <benoit.farley@gmail.com>
2018-12-05 10:09:29 -0500

    Removed, because Console replaced by Alain's package.

--- java/iutools-core/src/test/java/ca/inuktitutcomputing/core/console/ConsoleTest.java
+++ java/iutools-core/src/test/java/ca/inuktitutcomputing/core/console/ConsoleTest.java
@@ -9,11 +9,8 @@ import java.util.Set;
 import org.junit.Assert;
 import org.junit.Test;
 
-import ca.pirurvik.iutools.console.Console;
-import ca.pirurvik.iutools.console.ConsoleException;
-import ca.pirurvik.iutools.console.InvalidArgumentConsoleException;
-import ca.pirurvik.iutools.console.MiscellaneousConsoleException;
-import ca.pirurvik.iutools.console.UnknownFileConsoleException;
+import ca.inuktitutcomputing.core.console.Console;
+import ca.nrc.ui.commandline.CommandLineException;
 
 public class ConsoleTest {
 	
@@ -29,85 +26,27 @@ public class ConsoleTest {
 			// Use Console to launch an operation.
 			//
 			// On the command line:
-			// java -cp ... ca.pirurvik.iutools.console.Console name arguments*
+			// java -cp ... ca.inuktitutcomputing.console.Console name arguments*
 			//
 			// where name: name of the operation (for example: search_trie)
 			//       arguments: one or more arguments, depending on the operation
 			//
-			Console.main(new String[]{"test","argument1","argument2"});
-		} catch (ConsoleException e) {
+			String trieFilePath = "/Users/benoitfarley/Documents/git_repositories/iutools/java/iutools-data/src/test/trie_dump_with_error.txt";
+			Console.main(new String[]{"search_trie","-trie-file",trieFilePath});
+		} catch (Exception e) {
 		}
 	}
 	
-	@Test
-	public void test__Console__test() {
-		try {
-			//
-			// Use Console to launch an operation.
-			//
-			// On the command line:
-			// java -cp ... ca.pirurvik.iutools.console.Console name arguments*
-			//
-			// where name: name of the operation (for example: search_trie)
-			//       arguments: one or more arguments, depending on the operation
-			//
-			Console.main(new String[]{"test","argument1","argument2"});
-		} catch (ConsoleException e) {
-			assertFalse("There should not be an exception with this call.",true);
-		}
-	}
-
-	@Test(expected=MiscellaneousConsoleException.class)
-	public void test__Console__unknown_method() throws MiscellaneousConsoleException {
-		try {
-			Console.main(new String[]{"wabadoo","whatever","argument2"});
-		} catch(ConsoleException e) {
-			if (e.getClass().equals(MiscellaneousConsoleException.class))
-				throw (MiscellaneousConsoleException)e;
-		}
-	}
-
-	@Test(expected=InvalidArgumentConsoleException.class)
-	public void test__Console__search_trie__bad_argument() throws InvalidArgumentConsoleException {
-		try {
-			Console.main(new String[]{"search_trie","whatever","argument2"});
-		} catch(ConsoleException e) {
-			if (e.getClass().equals(InvalidArgumentConsoleException.class))
-				throw (InvalidArgumentConsoleException)e;
-		}
-	}
-
-	@Test(expected=UnknownFileConsoleException.class)
-	public void test__Console__search_trie__unknown_file() throws UnknownFileConsoleException {
-		try {
-			Console.main(new String[]{"search_trie","-trie-file","argument2"});
-		} catch(ConsoleException e) {
-			if (e.getClass().equals(UnknownFileConsoleException.class))
-				throw (UnknownFileConsoleException)e;
-		}
-	}
-
 	@Test(expected=ConsoleException.class)
 	public void test__Console__search_trie__file_with_error() throws ConsoleException {
 		try {
 			String trieFilePath = "/Users/benoitfarley/Documents/git_repositories/iutools/java/iutools-data/src/test/trie_dump_with_error.txt";
 			Console.main(new String[]{"search_trie","-trie-file",trieFilePath});
-		} catch(ConsoleException e) {
+		} catch(Exception e) {
 			if (e.getClass().equals(ConsoleException.class))
 				throw (ConsoleException)e;
-		} catch(Exception e) {
-			e.getStackTrace();
-		}
+		} 
 	}
 
-	@Test
-	public void test__Console__search_trie_one__good_arguments() {
-		try {
-			String trieFilePath = "/Users/benoitfarley/Documents/git_repositories/iutools/java/iutools-data/src/test/trie_dump.txt";
-			Console.main(new String[]{"search_trie_one","-trie-file",trieFilePath,"-surfaceform","takulaaq"});
-		} catch(ConsoleException e) {
-			assertFalse("There should not be an exception with this call.",true);
-		}
-	}
 
 }
