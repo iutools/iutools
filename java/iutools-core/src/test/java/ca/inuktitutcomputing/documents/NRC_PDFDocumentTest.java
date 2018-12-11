bae5c4bc9bb5da5b888597e43e25b87aeb95f60b
benoitfarley <benoit.farley@gmail.com>
2018-12-11 10:35:08 -0500

    Classes in ca.inuktitutcomputing.documents for PDF and DOC files processing

---
+++ java/iutools-core/src/test/java/ca/inuktitutcomputing/documents/NRC_PDFDocumentTest.java
@@ -0,0 +1,35 @@
+package ca.inuktitutcomputing.documents;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import ca.inuktitutcomputing.config.IUConfig;
+import ca.inuktitutcomputing.documents.NRC_PDFDocument;
+import ca.nrc.config.ConfigException;
+import junit.framework.TestCase;
+
+public class NRC_PDFDocumentTest extends TestCase {
+
+	public void testGetContents() throws ConfigException {
+		String pdfURLName = "file:///"+IUConfig.getIUDataPath()+"src/test/A-03763i_90.pdf";
+		try {
+			NRC_PDFDocument doc = new NRC_PDFDocument(pdfURLName);
+			String contents = doc.getContents();
+			String[] fontNames = doc.getAllFontsNames();
+			System.out.println("font names: "+Arrays.toString(fontNames));
+			System.out.println(contents);
+			String targetPattern = "Z\\?m4f5\\s+WJmJ5 g4yCsti4\\s+kNo8i Z\\?m4fi9l Wp5yC6t4f5,\\s+";
+			Pattern p = Pattern.compile(targetPattern);
+			Matcher mp = p.matcher(contents);
+			assertTrue("Mauvais résultat",mp.find());
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
