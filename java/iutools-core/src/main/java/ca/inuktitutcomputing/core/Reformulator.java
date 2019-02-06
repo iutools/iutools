f41a36901da83339de68822b9994e4bda3377dc3
benoitfarley <benoit.farley@gmail.com>
2019-02-06 12:14:20 -0500

    Class for reformulations

---
+++ java/iutools-core/src/main/java/ca/inuktitutcomputing/core/Reformulator.java
@@ -0,0 +1,65 @@
+package ca.inuktitutcomputing.core;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Vector;
+
+import ca.nrc.datastructure.trie.TrieNode;
+
+public class Reformulator {
+	
+	public CompiledCorpus compiledCorpus;
+	public int numberOfReformulations = 5;
+	
+	public Reformulator(CompiledCorpus _compiledCorpus) {
+		this.compiledCorpus = _compiledCorpus;
+	}
+	
+	public String[] getReformulations(String word) throws Exception {
+		String[] segments;
+		try {
+			TrieNode[] mostFrequentTerminalsForWord;
+			segments = this.compiledCorpus.getSegmenter().segment(word);
+			if (segments==null)
+				return null;
+			
+			TrieNode node = this.compiledCorpus.trie.getNode(segments);			
+			if (node==null)
+				mostFrequentTerminalsForWord = new TrieNode[] {};
+			else
+				mostFrequentTerminalsForWord = node.getMostFrequentTerminals(this.numberOfReformulations);
+			
+			return __getReformulations(mostFrequentTerminalsForWord, segments);
+			
+		} catch (Exception e) {
+			return null;
+		}
+	}
+	
+	public String[] __getReformulations(TrieNode[] mostFrequentTerminalsForReformulations, String[] segments) {
+		if (segments.length == 0 || mostFrequentTerminalsForReformulations.length == this.numberOfReformulations) {
+			String[] mostFrequentReformulations = new String[mostFrequentTerminalsForReformulations.length];
+			for (int i=0; i<mostFrequentTerminalsForReformulations.length; i++)
+				mostFrequentReformulations[i] = mostFrequentTerminalsForReformulations[i].getSurfaceForm();
+			return mostFrequentReformulations;
+		}
+		else {
+			// back one node
+			segments = Arrays.copyOfRange(segments,0,segments.length-1);
+			if (segments.length != 0) {
+				TrieNode node = this.compiledCorpus.trie.getNode(segments);
+				TrieNode[] mostFrequentTerminalsForNode = node.getMostFrequentTerminals(
+					this.numberOfReformulations - mostFrequentTerminalsForReformulations.length,
+					mostFrequentTerminalsForReformulations);
+				ArrayList<TrieNode> newMostFrequentTerminalsForReformulationsAL = new ArrayList<TrieNode>();
+				newMostFrequentTerminalsForReformulationsAL.addAll(Arrays.asList(mostFrequentTerminalsForReformulations));
+				newMostFrequentTerminalsForReformulationsAL.addAll(Arrays.asList(mostFrequentTerminalsForNode));
+				TrieNode[] newMostFrequentTerminalsForReformulations = (TrieNode[])newMostFrequentTerminalsForReformulationsAL.toArray(new TrieNode[] {});
+				return __getReformulations(newMostFrequentTerminalsForReformulations, segments);
+			} else
+				return __getReformulations(mostFrequentTerminalsForReformulations, segments);
+		}
+	}
+
+}
---
+++ java/iutools-core/src/test/java/ca/inuktitutcomputing/core/ReformulatorTest.java
@@ -0,0 +1,76 @@
+package ca.inuktitutcomputing.core;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Test;
+
+import ca.inuktitutcomputing.config.IUConfig;
+import ca.nrc.datastructure.trie.StringSegmenter_IUMorpheme;
+
+class ReformulatorTest {
+
+	@Test
+	void test_getReformulations() throws Exception {
+		String[] words = new String[] {
+				"nuna", "nunait", 
+				"iglu", "iglumut", "iglumut", "iglumut", "iglumik", "iglu",
+				"takujuq", "takujumajunga"
+		};
+		File dir = new File(IUConfig.getIUDataPath()+"src/test/temp");
+		dir.mkdir();
+		String corpusDir = dir.getAbsolutePath();
+		BufferedWriter bw = new BufferedWriter(new FileWriter(
+				new File(corpusDir+"/corpusText.txt")));
+		bw.write(String.join(" ", words));
+		bw.close();
+        CompiledCorpus compiledCorpus = new CompiledCorpus(StringSegmenter_IUMorpheme.class.getName());
+        compiledCorpus.compileCorpusFromScratch(corpusDir);
+        FileUtils.deleteDirectory(dir);
+        
+        Reformulator reformulator = new Reformulator(compiledCorpus);
+        String[] reformulations = reformulator.getReformulations("iglu");
+        String[] expected = new String[] {"iglumut","iglu","iglumik"};
+        
+        
+        assertArrayEquals("The reformulations are not correct.",expected,reformulations);
+	}
+
+	@Test
+	void test_getReformulations__Case_with_stepping_back_one_node() throws Exception {
+		String[] words = new String[] {
+				"nuna", "nunait", 
+				"iglu", "iglumut", "iglumut", "iglumut", "iglumik", "iglu", "iglumiutaq",
+				"takujuq", "takujumajunga"
+		};
+		File dir = new File(IUConfig.getIUDataPath()+"src/test/temp");
+		dir.mkdir();
+		String corpusDir = dir.getAbsolutePath();
+		BufferedWriter bw = new BufferedWriter(new FileWriter(
+				new File(corpusDir+"/corpusText.txt")));
+		bw.write(String.join(" ", words));
+		bw.close();
+        CompiledCorpus compiledCorpus = new CompiledCorpus(StringSegmenter_IUMorpheme.class.getName());
+        compiledCorpus.compileCorpusFromScratch(corpusDir);
+        FileUtils.deleteDirectory(dir);
+        
+        Reformulator reformulator = new Reformulator(compiledCorpus);
+        String[] reformulations = reformulator.getReformulations("iglumiutaq");
+        String[] expected = new String[] {"iglumut","iglu","iglumik","iglumiutaq"};
+        
+        
+        assertEquals("The number of reformulations returned is wrong.",4,reformulations.length);
+        List<String> reformulationsList = Arrays.asList(reformulations);
+        for (String expectedRef : expected)
+        	assertTrue("The word '"+expectedRef+"' should have been returned.",reformulationsList.contains(expectedRef));
+	}
+
+}
