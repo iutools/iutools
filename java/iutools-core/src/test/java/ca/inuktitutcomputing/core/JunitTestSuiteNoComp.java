d48ade3b4bd2285d9132f607f5a27d05af1934ab
benoitfarley <benoit.farley@gmail.com>
2019-03-04 12:29:06 -0500

    Test suites

---
+++ java/iutools-core/src/test/java/ca/inuktitutcomputing/core/JunitTestSuiteNoComp.java
@@ -0,0 +1,22 @@
+package ca.inuktitutcomputing.core;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses(
+{
+   CompiledCorpus_IUMorphemeTest.class,
+   CompiledCorpusTest.class,
+   CorpusReader_DirectoryTest.class,
+   QueryExpanderEvaluatorTest.class,
+   QueryExpanderTest.class
+}
+)
+
+
+public class JunitTestSuiteNoComp {   
+
+} 
---
+++ java/iutools-core/src/test/java/ca/nrc/datastructure/trie/JunitTestSuite.java
@@ -0,0 +1,20 @@
+package ca.nrc.datastructure.trie;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses(
+{
+   StringSegmenter_IUMorphemeTest.class,
+   TrieNodeTest.class,
+   TrieTest.class
+}
+)
+
+
+public class JunitTestSuite {   
+
+} 
