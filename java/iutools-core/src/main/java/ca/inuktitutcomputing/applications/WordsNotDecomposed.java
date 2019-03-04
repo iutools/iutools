c5746e2439eaa94efbccd42a1e284dee267e3cd1
benoitfarley <benoit.farley@gmail.com>
2019-03-04 12:31:13 -0500

    Returns words of the Hansard not decomposed during corpus compilation

---
+++ java/iutools-core/src/main/java/ca/inuktitutcomputing/applications/WordsNotDecomposed.java
@@ -0,0 +1,37 @@
+package ca.inuktitutcomputing.applications;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import com.google.gson.Gson;
+
+import ca.inuktitutcomputing.core.CompiledCorpus;
+
+public class WordsNotDecomposed {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		String compilationFilePath = args[0];
+		FileReader fr = new FileReader(compilationFilePath);
+		CompiledCorpus compiledCorpus = new Gson().fromJson(fr, CompiledCorpus.class);
+		HashMap<String,Long> wordsThatWereNotDecomposed = compiledCorpus.getWordsThatFailedSegmentationWithFreqs();
+		Object[][] objs = new Object[wordsThatWereNotDecomposed.size()][2];
+		Iterator<String> iterator = wordsThatWereNotDecomposed.keySet().iterator();
+		int i = 0;
+	    while(iterator.hasNext()) {
+	    	String word = iterator.next();
+	        objs[i++] = new Object[] {word,wordsThatWereNotDecomposed.get(word)};
+	    }
+	    Arrays.sort(objs, (Object[] a, Object[] b) -> {
+	    	return ((Long)b[1]).compareTo((Long)a[1]);
+	    });
+	    int j=0;
+	    for (Object[] o : objs) {
+	    	System.out.println(o[0]+","+o[1]);
+	    	if (j++ == 100) break;
+	    }
+	}
+
+}
