4ed09674e48a660f7e97ffc44b717ae3da42b47a
benoitfarley <benoit.farley@gmail.com>
2019-03-09 12:50:59 -0500

    changes of parameters to get other results

--- java/iutools-core/src/main/java/ca/inuktitutcomputing/applications/WordsNotDecomposed.java
+++ java/iutools-core/src/main/java/ca/inuktitutcomputing/applications/WordsNotDecomposed.java
@@ -12,14 +12,16 @@ import ca.inuktitutcomputing.core.CompiledCorpus;
 
 public class WordsNotDecomposed {
 	
-	static private int limite = 200;
+	static private int limite = -1; //200;
 
 	public static void main(String[] args) throws FileNotFoundException {
 		String compilationFilePath = args[0];
 		FileReader fr = new FileReader(compilationFilePath);
 		CompiledCorpus compiledCorpus = new Gson().fromJson(fr, CompiledCorpus.class);
 		HashMap<String,Long> wordsThatWereNotDecomposed = compiledCorpus.getWordsThatFailedSegmentationWithFreqs();
-		Object[][] objs = new Object[wordsThatWereNotDecomposed.size()][2];
+		int nWords = wordsThatWereNotDecomposed.size();
+		Object[][] objs = new Object[nWords][2];
+		HashMap<Long,Long> freqs = new HashMap<Long,Long>();
 		Iterator<String> iterator = wordsThatWereNotDecomposed.keySet().iterator();
 		int i = 0;
 	    while(iterator.hasNext()) {
@@ -31,9 +33,30 @@ public class WordsNotDecomposed {
 	    });
 	    int j=0;
 	    for (Object[] o : objs) {
+	    	Long freq = (Long)o[1];
 	    	System.out.println(o[0]+","+o[1]);
+	    	if (freqs.containsKey(freq))
+	    		freqs.put(freq, freqs.get(freq)+1);
+	    	else
+	    		freqs.put(freq, new Long(1));
 	    	if (j++ == limite) break;
 	    }
+	    System.out.println("Nb. mots: "+nWords);
+	    
+	    System.out.println("\n#occ. : #words\n");
+	    
+	    Object[][] frqs = new Object[freqs.size()][2];
+		Iterator<Long> iteratorfrq = freqs.keySet().iterator();
+		int k = 0;
+	    while(iteratorfrq.hasNext()) {
+	    	Long freq = iteratorfrq.next();
+	        frqs[k++] = new Object[] {freq,freqs.get(freq)};
+	    }
+	    Arrays.sort(frqs, (Object[] a, Object[] b) -> {
+	    	return ((Long)b[0]).compareTo((Long)a[0]);
+	    });
+	    for (k=0; k<frqs.length; k++)
+	    	System.out.println(frqs[k][0]+" : "+frqs[k][1]);
 	}
 
 }
