270d3ea30b5797db7abf7043b0124dac2ac3eed8
benoitfarley <benoit.farley@gmail.com>
2019-03-11 21:23:45 -0400

    Spell checker

---
+++ java/iutools-core/src/main/java/ca/inuktitutcomputing/core/SpellChecker.java
@@ -0,0 +1,190 @@
+package ca.inuktitutcomputing.core;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import ca.nrc.datastructure.Pair;
+import ca.inuktitutcomputing.utilities.Levenshtein;
+
+public class SpellChecker {
+	
+	protected int MAX_SEQ_LEN = 5;
+	protected int MAX_CANDIDATES = 100;
+	protected String allWords = ",,";
+	protected Map<String,Long> idfStats = new HashMap<String,Long>();
+	
+
+	public void addCorrectWord(String word) {
+		System.out.println("-- addCorrectWord: word="+word);;
+		allWords += word+",,";
+		Set<String> seqSeen = new HashSet<String>();
+		try {
+		for (int seqLen = 1; seqLen <= MAX_SEQ_LEN; seqLen++) {
+			for (int  start=0; start <= word.length() - seqLen; start++) {
+				String charSeq = word.substring(start, start+seqLen);
+				System.out.println("-- addCorrectWord:    charSeq="+charSeq);;
+				if (!seqSeen.contains(charSeq)) {
+					updateSequenceIDF(charSeq);
+				}
+				seqSeen.add(charSeq);
+			}
+		}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void updateSequenceIDF(String charSeq) {
+		if (!idfStats.containsKey(charSeq)) {
+			idfStats.put(charSeq, new Long(0));
+		}
+		idfStats.put(charSeq, idfStats.get(charSeq)+1);
+		System.out.println("-- udpateSequenceIDF:    upon exit, idfStats.get(charSeq)="+idfStats.get(charSeq));
+	}
+
+	public void saveToFile(File checkerFile) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public SpellChecker readFromFile(File checkerFile) {
+		return this;
+	}
+
+	public List<String> correct(String badWord) {
+		return correct(badWord,-1);
+	}
+
+	public Long idf(String charSeq) {
+		Long val = new Long(0);
+		if (idfStats.containsKey(charSeq)) {
+			val = idfStats.get(charSeq);
+		}
+		return val;
+	}
+
+	public List<String> correct(String badWord, int maxCorrections) {
+		
+		Set<String> candidates = firstPassCandidates(badWord);
+		List<Pair<String,Double>> candidatesWithSim = computeCandidateSimilarities(badWord, candidates);
+		List<String> corrections = sortCandidatesBySimilarity(candidatesWithSim);
+ 		if (maxCorrections== -1)
+ 			return corrections;
+ 		else
+ 			return corrections.subList(0, maxCorrections>corrections.size()? corrections.size() : maxCorrections);
+	}
+
+	private List<String> sortCandidatesBySimilarity(List<Pair<String, Double>> candidatesWithSim) {
+		Iterator<Pair<String, Double>> iteratorCand = candidatesWithSim.iterator();
+		while (iteratorCand.hasNext()) {
+			Pair<String,Double> pair = iteratorCand.next();
+			System.out.println("-- sortCandidatesBySimiliraty (1): "+"candidate: "+pair.getFirst()+" ; similarity="+pair.getSecond());
+		}
+	
+		Collections.sort(candidatesWithSim, (Pair<String,Double> p1, Pair<String,Double> p2) -> {
+			return p1.getSecond().compareTo(p2.getSecond());
+		});
+		List<String> candidates = new ArrayList<String>();
+		iteratorCand = candidatesWithSim.iterator();
+		while (iteratorCand.hasNext()) {
+			Pair<String,Double> pair = iteratorCand.next();
+			candidates.add(pair.getFirst());
+			System.out.println("-- sortCandidatesBySimiliraty (2): "+"candidate: "+pair.getFirst()+" ; similarity="+pair.getSecond());
+		}
+		return candidates;
+	}
+
+	protected List<Pair<String, Double>> computeCandidateSimilarities(String badWord, Set<String> candidates) {
+		List<Pair<String,Double>> candidateSimilarities = new ArrayList<Pair<String,Double>>();
+		Iterator<String> iterator = candidates.iterator();
+		while (iterator.hasNext()) {
+			String candidate = iterator.next();
+			double similarity = computeCandidateSimilarity(badWord,candidate);
+			candidateSimilarities.add(new Pair<String,Double>(candidate,new Double(similarity)));
+			System.out.println("-- computeCandidateSimilarities:    "+"candidate: "+candidate+" ; similarity="+similarity);
+		}
+		return candidateSimilarities;
+	}
+
+	private double computeCandidateSimilarity(String badWord, String candidate) {
+		// TODO Auto-generated method stub
+		int distance = Levenshtein.distance(candidate,badWord);
+		return (double)distance;
+	}
+
+	public Set<String> firstPassCandidates(String badWord) {
+		Set<String> candidates = new HashSet<String>();
+		List<Pair<String,Long>> rarest = rarestSequencesOf(badWord);
+		while (!rarest.isEmpty()) {
+			Pair<String,Long> currSeqInfo = rarest.remove(0);
+			Set<String> wordsWithSequence = wordsContainingSequ(currSeqInfo.getFirst());
+			candidates.addAll(wordsWithSequence);
+			if (candidates.size() >= MAX_CANDIDATES) {
+				if (!rarest.isEmpty()) {
+					Pair<String,Long> nextSeqInfo = rarest.get(0);
+					if (currSeqInfo.getSecond() != nextSeqInfo.getSecond()) {
+						// The next sequence is just as rare as the current one, so 
+						// keep going.
+						continue;
+					} else {
+						break;
+					}
+				}
+			}
+		}
+		
+		return candidates;
+	}
+
+	protected Set<String> wordsContainingSequ(String seq) {
+		Pattern p = Pattern.compile(",([^,]*"+seq+"[^,]*),");
+		Matcher m = p.matcher(allWords);
+		Set wordsWithSeq = new HashSet();
+		while (m.find())
+			wordsWithSeq.add(m.group(1));
+		return wordsWithSeq;
+	}
+
+	public List<Pair<String, Long>> rarestSequencesOf(String string) {
+		List<Pair<String,Long>> listOfRarest = new ArrayList<Pair<String,Long>>();
+		// TODO:
+		for (int seqLen = 1; seqLen <= MAX_SEQ_LEN; seqLen++) {
+			for (int  start=0; start <= string.length() - seqLen; start++) {
+				String charSeq = string.substring(start, start+seqLen);
+				Long idf = idf(charSeq);
+				System.out.println("-- rarestSequencesOf:    charSeq="+charSeq+" ("+idf+")");;
+				if (idf != 0) {
+					Pair<String,Long> pair = new Pair(charSeq,idf);
+					if ( !listOfRarest.contains(pair) )
+						listOfRarest.add(pair);
+				}
+			}
+		}
+		//Object[] rarest = listOfRarest.toArray(new Object[] {});
+		//Arrays.sort(rarest,(Object p1, Object p2) -> {
+		//	return ((Pair<String,Long>)p1).getSecond().compareTo(((Pair<String,Long>)p2).getSecond());
+		//});
+		Collections.sort(listOfRarest,(Object p1, Object p2) -> {
+			return ((Pair<String,Long>)p1).getSecond().compareTo(((Pair<String,Long>)p2).getSecond());
+		});
+		for (int il=0; il < listOfRarest.size(); il++)
+			System.out.println("-- rarestSequencesOf:    rarest["+il+"]= "+
+					((Pair<String,Long>)listOfRarest.get(il)).getFirst()+" ("+
+					((Pair<String,Long>)listOfRarest.get(il)).getSecond()+")");
+		
+		return listOfRarest;
+		
+	}
+
+}
---
+++ java/iutools-core/src/test/java/ca/inuktitutcomputing/core/SpellCheckerTest.java
@@ -0,0 +1,184 @@
+package ca.inuktitutcomputing.core;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Test;
+
+import ca.nrc.datastructure.Pair;
+
+import org.junit.*;
+
+public class SpellCheckerTest {
+	
+	private SpellChecker checker = new SpellChecker();
+	
+	@Before
+	public void setUp() {
+		checker.addCorrectWord("inuktut");
+		checker.addCorrectWord("inuk");
+		checker.addCorrectWord("inukshuk");
+		checker.addCorrectWord("nunavut");
+	}
+
+	@Test
+	public void test__SpellChecker__Synopsis() throws IOException {
+		//
+		// Before you can use a spell checker, you must first build its
+		// dictioanary of correct words.
+		// 
+		// For example
+		//
+		SpellChecker checker = new SpellChecker();
+		checker.addCorrectWord("inuktut");
+		checker.addCorrectWord("inuk");
+		checker.addCorrectWord("inukshuk");
+		checker.addCorrectWord("nunavut");
+		// etc...
+		
+		//
+		// Once you have built its dictionary, you can save the 
+		// SpellChecker to file.
+		//
+		// Note: For the needs of this test, we use a temporary file that
+		// will be deleted upon exit. But in a real use case, you would
+		// save it to a permanent file.
+		//
+		File checkerFile = File.createTempFile("checker", "json");
+		checkerFile.deleteOnExit();
+		checker.saveToFile(checkerFile);
+		
+		//
+		// Later on, you can recreate the spell checker from file
+		//
+		checker = new SpellChecker().readFromFile(checkerFile);
+		
+		//
+		// You can then use the check to get plausible corrections for
+		// a badly spelled word.
+		//
+		int nCorrection = 5;
+		List<String> corrections = checker.correct("inushuk", 5);
+
+	}
+	
+	/**********************************
+	 * VERIFICATION TESTS
+	 **********************************/
+	
+	@Test
+	public void test__addCorrectWord__HappyPath() {
+		SpellChecker checker = new SpellChecker();
+		
+		Assert.assertFalse(containsWord("inuktut", checker));
+		checker.addCorrectWord("inuktut");
+		Assert.assertTrue(containsWord("inuktut", checker));
+		
+		Assert.assertFalse(containsWord("nunavut", checker));
+		checker.addCorrectWord("nunavut");
+		Assert.assertTrue(containsWord("nunavut", checker));
+	}
+	
+	@Test
+	public void test__idf__HappyPath() {
+		Assert.assertEquals("IDF was wrong for sequence that starts words ('inu')", new Long(3), checker.idf("inu"));
+		Assert.assertEquals("IDF was wrong for sequence at the middle of words ('ks')", new Long(1), checker.idf("ks"));
+		Assert.assertEquals("IDF was wrong for sequence at the end of words ('ktut')", new Long(1), checker.idf("ktut"));
+		Assert.assertEquals("IDF was wrong for sequence with > 5 chars", new Long(0), checker.idf("nunavu"));
+		Assert.assertEquals("IDF was wrong for sequence with =5 chars", new Long(1), checker.idf("unavu"));
+		Assert.assertEquals("IDF was wrong for non-existant sequence",new Long(0), checker.idf("blah"));
+	}
+
+	@Test
+	public void test__rarestSequencesOf__HappyPath() {
+		List<Pair<String,Long>> rarest = checker.rarestSequencesOf("inukkshuk");
+		List<Pair<String,Long>> expected = new ArrayList<Pair<String,Long>>();
+		expected.add(new Pair<String,Long>("s",new Long(1)));
+		expected.add(new Pair<String,Long>("h",new Long(1)));
+		expected.add(new Pair<String,Long>("ks",new Long(1)));
+		expected.add(new Pair<String,Long>("sh",new Long(1)));
+		expected.add(new Pair<String,Long>("hu",new Long(1)));
+		expected.add(new Pair<String,Long>("ksh",new Long(1)));
+		expected.add(new Pair<String,Long>("shu",new Long(1)));
+		expected.add(new Pair<String,Long>("huk",new Long(1)));
+		expected.add(new Pair<String,Long>("kshu",new Long(1)));
+		expected.add(new Pair<String,Long>("shuk",new Long(1)));
+		expected.add(new Pair<String,Long>("kshuk",new Long(1)));
+		expected.add(new Pair<String,Long>("i",new Long(3)));
+		expected.add(new Pair<String,Long>("k",new Long(3)));
+		expected.add(new Pair<String,Long>("in",new Long(3)));
+		expected.add(new Pair<String,Long>("uk",new Long(3)));
+		expected.add(new Pair<String,Long>("inu",new Long(3)));
+		expected.add(new Pair<String,Long>("nuk",new Long(3)));
+		expected.add(new Pair<String,Long>("inuk",new Long(3)));
+		expected.add(new Pair<String,Long>("n",new Long(4)));
+		expected.add(new Pair<String,Long>("u",new Long(4)));
+		expected.add(new Pair<String,Long>("nu",new Long(4)));
+		Assert.assertEquals("The number of rarest sequences is wrong.", 21, rarest.size());
+		for (int i=0; i<expected.size(); i++) {
+			Assert.assertEquals(i+". The string of the sequence is wrong.",
+					((Pair<String,Long>)expected.get(i)).getFirst(),
+					((Pair<String,Long>)rarest.get(i)).getFirst());
+			Assert.assertEquals(i+". The string of the sequence is wrong.",
+					((Pair<String,Long>)expected.get(i)).getSecond(),
+					((Pair<String,Long>)rarest.get(i)).getSecond());
+		}
+	}
+	
+	@Test
+	public void test__wordsContainingSequ() {
+		String seq = "nuk";
+		Set<String> wordsWithSeq = checker.wordsContainingSequ(seq);
+		System.out.println(checker.allWords);
+		Assert.assertEquals("The number of words containing the sequence is wrong.", 3, wordsWithSeq.size());
+		String[] expected = new String[] {"inukshuk","inuk","inuktut"};
+		for (int i=0; i<expected.length; i++)
+			Assert.assertTrue("The element "+expected[i]+" is not in the returned list.",wordsWithSeq.contains(expected[i]));
+	}
+	
+	/*
+	 *  checker.addCorrectWord("inuktut");
+		checker.addCorrectWord("inuk");
+		checker.addCorrectWord("inukshuk");
+		checker.addCorrectWord("nunavut");
+
+	 */
+	@Test
+	public void test__firstPassCandidates() {
+		String badWord = "inukkshuk";
+		Set<String> candidates = checker.firstPassCandidates(badWord);
+		Assert.assertEquals("The number of candidates is wrong.", 4, candidates.size());
+		String[] expected = new String[] {"inukshuk","inuk","inuktut","nunavut"};
+		for (int i=0; i<expected.length; i++)
+			Assert.assertTrue("The element "+expected[i]+" is not in the returned list.",candidates.contains(expected[i]));
+	}
+	
+	@Test
+	public void test__correct() {
+		List<String> corrections = checker.correct("inukkshuk");
+		String[] expected = new String[] {"inukshuk","inuktut","inuk","nunavut"};
+		Assert.assertEquals("The number of candidates is wrong.", 4, corrections.size());
+		for (int i=0; i<expected.length; i++) {
+			Assert.assertEquals("The element "+i+" of the list of corrections is not right.",expected[i],corrections.get(i));
+		}
+	}
+
+	/**********************************
+	 * TEST HELPERS
+	 **********************************/
+	
+	private boolean containsWord(String word, SpellChecker checker) {
+		boolean answer = false;
+		if (checker.allWords.indexOf(","+word+",") >= 0) {
+			answer = true;
+		}
+		return answer;
+	}
+
+}
