9903ff439cabb08abce4c3f54d06a3663bb43d68
desiletsa <alain.desilets@nrc-cnrc.gc.ca>
2019-04-29 09:52:53 -0400

    New method SearchInputs.getQuerySyllabic()

--- java/iutools-apps/src/main/java/ca/pirurvik/iutools/webservice/SearchInputs.java
+++ java/iutools-apps/src/main/java/ca/pirurvik/iutools/webservice/SearchInputs.java
@@ -1,9 +1,36 @@
 package ca.pirurvik.iutools.webservice;
 
-public class SearchInputs extends ServiceInputs {
-	String query = null;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import ca.inuktitutcomputing.script.Roman;
+import ca.inuktitutcomputing.script.Syllabics;
+import ca.inuktitutcomputing.script.TransCoder;
+
+public class SearchInputs extends ServiceInputs {
+	public String query = "";
+	private String _querySyllabic = null;
+
+	public SearchInputs() {
+	}
+	
 	public SearchInputs(String _query) {
 		this.query = _query;
 	}
+	
+	@JsonIgnore
+	public String getQuerySyllabic() {
+		String _querySyllabic = this.query;
+		if (_querySyllabic != null) {
+			Matcher matcher = Pattern.compile("[a-zA-z]").matcher(_querySyllabic);
+			if (matcher.find()) {
+				_querySyllabic = TransCoder.romanToUnicode(_querySyllabic);
+			}
+			
+		}
+		return _querySyllabic;
+	}
+	
 }
