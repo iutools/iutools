6a9c73981be2dda397209e0bc6767a3c9f27f020
benoitfarley <benoit.farley@gmail.com>
2019-05-30 16:09:42 -0400

    With regards to the throwing of exception

--- java/iutools-core/src/test/java/ca/pirurvik/iutools/MorphemeExtractorTest.java
+++ java/iutools-core/src/test/java/ca/pirurvik/iutools/MorphemeExtractorTest.java
@@ -1,130 +1,161 @@
 package ca.pirurvik.iutools;
 
-import static org.junit.Assert.*;
-
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
 
 import org.junit.Test;
 
 import ca.nrc.datastructure.Pair;
-import ca.nrc.json.PrettyPrinter;
-import ca.pirurvik.iutools.SpellChecker;
+import ca.nrc.testing.AssertHelpers;
 
 import org.junit.*;
 
 public class MorphemeExtractorTest {
 	
 	private MorphemeExtractor morphemeExtractor = new MorphemeExtractor();
+	private MockCompiledCorpus mockCompiledCorpus;
 	
 	@Before
-	public void setUp() {
-		String dictionary = ",,"
-				+"word1:{root1/idr1}{affix1/idaff1},,"
-				+"word2:{root2/idr2}{affix21/idaff21},,"
-				+"word3:{root3/idr3}{affix21/idaff21}{affix32/idaff31},,"
-				+"word4:{root1/idr1}{affix4/idaff4},,"
-			;
-		this.morphemeExtractor.setDictionary(dictionary);
+	public void setUp() throws IOException, ClassNotFoundException, NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		HashMap<String,String> dictionary = new HashMap<String,String>();
+		dictionary.put("inuit", "{root1/idr1} {affix1/idaff1}");
+		dictionary.put("nunami", "{root2/idr2} {affix21/idaff21}");
+		dictionary.put("iglumik", "{root3/idr3} {affix21/idaff21} {affix32/idaff31}");
+		dictionary.put("inuksuk", "{root1/idr1} {affix4/idaff4}");
+		mockCompiledCorpus = new MockCompiledCorpus();
+		// The MockCompiledCorpus's segmenter will use this dictionary instead of calling 
+		// the morphological analyzer.
+		mockCompiledCorpus.setDictionary(dictionary);
+		String[] stringsOfWords = new String[] {
+				"inuit nunami iglumik inuksuk"
+				};
+		String corpusDirPathname = createTemporaryCorpusDirectory(stringsOfWords);
+        try {
+        	mockCompiledCorpus.compileCorpusFromScratch(corpusDirPathname);
+        } catch(Exception e) {
+        	System.err.println("Exiting from compiler");
+        	System.err.println("because: "+e.getMessage());
+        	System.exit(1);
+        }
+        morphemeExtractor.useCorpus(mockCompiledCorpus);
 	}
 
-	@Test (expected=IOException.class)
-	public void test__SpellChecker__Synopsis() throws IOException {
+	@Test
+	public void test__MorphemeExtractor__Synopsis() throws Exception {
 		//
 		MorphemeExtractor morphemeExtractor = new MorphemeExtractor();
 
-		// Before you can use a morpheme extractor, you must first build its
-		// dictioanary of decompositions.
+		// The morpheme extractor uses the word base of a compiled corpus
 		// 
 		// For example
 		//
-		File file = new File("/path/to/words/decompositions/file");
-		morphemeExtractor.useDictionary(file);
+		CompiledCorpus corpus = mockCompiledCorpus;
+		morphemeExtractor.useCorpus(corpus);
 		
 		//
 		// Once you have built its dictionary, you can look for words that
 		// contain a given morpheme.
-		String morpheme = "lauqsima";
+		String morpheme = "nunami";
 		List<MorphemeExtractor.Words> wordsForMorphemes = morphemeExtractor.wordsContainingMorpheme(morpheme);
 	}
 	
+	@Test(expected=Exception.class)
+	public void test__MorphemeExtractor__Synopsis__No_corpus_defined() throws Exception {
+		MorphemeExtractor morphemeExtractor = new MorphemeExtractor();
+		String morpheme = "nunami";
+		List<MorphemeExtractor.Words> wordsForMorphemes = morphemeExtractor.wordsContainingMorpheme(morpheme);
+	}
+		
 	/**********************************
 	 * VERIFICATION TESTS
+	 * @throws Exception 
 	 **********************************/
 	
 	@Test
-	public void test__wordsContainingMorpheme__root() {
+	public void test__wordsContainingMorpheme__root() throws Exception {
 		String morpheme = "root1";
 		List<MorphemeExtractor.Words> wordsForMorphemes = this.morphemeExtractor.wordsContainingMorpheme(morpheme);
 		Assert.assertTrue(wordsForMorphemes.size()==1);
-		List<String> words = wordsForMorphemes.get(0).words;	
-		List<String> expected = new ArrayList<String>();
-		expected.add("word1"); expected.add("word4");
+		List<Pair<String, Long>> words = wordsForMorphemes.get(0).words;	
+		List<Pair<String, Long>> expected = new ArrayList<Pair<String, Long>>();
+		expected.add(new Pair<String,Long>("inuit",new Long(1))); 
+		expected.add(new Pair<String,Long>("inuksuk",new Long(1)));
 		Assert.assertEquals(2, words.size());
-		Assert.assertTrue(words.contains(expected.get(0)));
-		Assert.assertTrue(words.contains(expected.get(1)));
+		AssertHelpers.assertDeepEquals("",expected,words);
 		
 		morpheme = "root2";
 		wordsForMorphemes = this.morphemeExtractor.wordsContainingMorpheme(morpheme);
 		Assert.assertTrue(wordsForMorphemes.size()==1);
 		words = wordsForMorphemes.get(0).words;
-		expected = new ArrayList<String>();
-		expected.add("word2");
+		expected = new ArrayList<Pair<String, Long>>();
+		expected.add(new Pair<String,Long>("nunami",new Long(1)));
 		Assert.assertEquals(1, words.size());
-		Assert.assertTrue(words.contains(expected.get(0)));
+		AssertHelpers.assertDeepEquals("",expected,words);
 		
-		String dictionary = ",,"
-				+"word1:{root1/idr1}{affix1/idaff1},,"
-				+"word2:{root2/idr2}{affix21/idaff21},,"
-				+"word3:{root3/idr3}{affix21/idaff21}{affix32/idaff31},,"
-				+"word4:{root1/idr1}{affix41/idaff411},,"
-				+"word5:{root3/idr3}{affix41/idaff411},,"
-				+"word6:{root2/idr2}{affix41/idaff412}{affix42/idaff42}"
-			;
-		this.morphemeExtractor.setDictionary(dictionary);
+		HashMap<String,String> dictionary = new HashMap<String,String>();
+		dictionary.put("inuit", "{root1/idr1} {affix1/idaff1}");
+		dictionary.put("nunami", "{root2/idr2} {affix21/idaff21}");
+		dictionary.put("iglumik", "{root3/idr3} {affix21/idaff21} {affix32/idaff31}");
+		dictionary.put("inuksuk", "{root1/idr1} {affix41/idaff411}");
+		dictionary.put("iglumut", "{root3/idr3} {affix41/idaff411}");
+		dictionary.put("nunatsiaq", "{root2/idr2} {affix41/idaff412} {affix42/idaff42}");
+		MockCompiledCorpus mockCompiledCorpus = new MockCompiledCorpus();
+		mockCompiledCorpus.setDictionary(dictionary);
+		String[] stringsOfWords = new String[] {
+				"inuit nunami iglumik inuksuk iglumut nunatsiaq"
+				};
+		String corpusDirPathname = createTemporaryCorpusDirectory(stringsOfWords);
+        try {
+        	mockCompiledCorpus.compileCorpusFromScratch(corpusDirPathname);
+        } catch(Exception e) {
+        	System.err.println("Exiting from compiler");
+        }
+        morphemeExtractor.useCorpus(mockCompiledCorpus);
 		morpheme = "affix41";
 		wordsForMorphemes = this.morphemeExtractor.wordsContainingMorpheme(morpheme);
 		Assert.assertTrue(wordsForMorphemes.size()==2);
 		
 		if (wordsForMorphemes.get(0).morphemeWithId.equals("affix41/idaff411")) {
 			words = wordsForMorphemes.get(0).words;
-			expected = new ArrayList<String>();
-			expected.add("word4"); expected.add("word5");
+			expected = new ArrayList<Pair<String, Long>>();
+			expected.add(new Pair<String, Long>("inuksuk", new Long(1))); 
+			expected.add(new Pair<String, Long>("iglumut", new Long(1)));
 			Assert.assertEquals(2, words.size());
-			Assert.assertTrue(words.contains(expected.get(0)));
-			Assert.assertTrue(words.contains(expected.get(1)));
+			AssertHelpers.assertDeepEquals("",expected,words);
 		
 			words = wordsForMorphemes.get(1).words;
-			expected = new ArrayList<String>();
-			expected.add("word6");
+			expected = new ArrayList<Pair<String, Long>>();
+			expected.add(new Pair<String, Long>("nunatsiaq",new Long(1)));
 			Assert.assertEquals(1, words.size());
-			Assert.assertTrue(words.contains(expected.get(0)));
+			AssertHelpers.assertDeepEquals("",expected,words);
 		} else {
 			words = wordsForMorphemes.get(1).words;
-			expected = new ArrayList<String>();
-			expected.add("word4"); expected.add("word5");
+			expected = new ArrayList<Pair<String, Long>>();
+			expected.add(new Pair<String, Long>("inuksuk", new Long(1))); 
+			expected.add(new Pair<String, Long>("iglumut", new Long(1)));
 			Assert.assertEquals(2, words.size());
-			Assert.assertTrue(words.contains(expected.get(0)));
-			Assert.assertTrue(words.contains(expected.get(1)));
+			AssertHelpers.assertDeepEquals("",expected,words);
 		
 			words = wordsForMorphemes.get(0).words;
-			expected = new ArrayList<String>();
-			expected.add("word6");
+			expected = new ArrayList<Pair<String, Long>>();
+			expected.add(new Pair<String, Long>("nunatsiaq", new Long(1)));
 			Assert.assertEquals(1, words.size());
-			Assert.assertTrue(words.contains(expected.get(0)));
+			AssertHelpers.assertDeepEquals("",expected,words);
 		}
 	}
 	
-	public void test__wordsContainingMorpheme__infix() {
+	public void test__wordsContainingMorpheme__infix() throws Exception {
 		String morpheme = "affix21";
 		List<MorphemeExtractor.Words> wordsForMorphemes = this.morphemeExtractor.wordsContainingMorpheme(morpheme);
 		Assert.assertTrue(wordsForMorphemes.size()==1);
-		List<String> words = wordsForMorphemes.get(0).words;	
+		List<Pair<String, Long>> words = wordsForMorphemes.get(0).words;	
 		List<String> expected = new ArrayList<String>();
 		expected.add("word2"); expected.add("word3");
 		Assert.assertEquals(2, words.size());
@@ -132,11 +163,11 @@ public class MorphemeExtractorTest {
 		Assert.assertTrue(words.contains(expected.get(1)));
 	}
 	
-	public void test__wordsContainingMorpheme__ending() {
+	public void test__wordsContainingMorpheme__ending() throws Exception {
 		String morpheme = "affix4";
 		List<MorphemeExtractor.Words> wordsForMorphemes = this.morphemeExtractor.wordsContainingMorpheme(morpheme);
 		Assert.assertTrue(wordsForMorphemes.size()==1);
-		List<String> words = wordsForMorphemes.get(0).words;	
+		List<Pair<String, Long>> words = wordsForMorphemes.get(0).words;	
 		List<String> expected = new ArrayList<String>();
 		expected.add("word4");
 		Assert.assertEquals(1, words.size());
@@ -148,4 +179,18 @@ public class MorphemeExtractorTest {
 	 * TEST HELPERS
 	 **********************************/
 	
+    private String createTemporaryCorpusDirectory(String[] stringOfWords) throws IOException {
+        File corpusDirectory = Files.createTempDirectory("").toFile();
+        corpusDirectory.deleteOnExit();
+        String corpusDirPath = corpusDirectory.getAbsolutePath();
+        for (int i=0; i<stringOfWords.length; i++) {
+        	File wordFile = new File(corpusDirPath+"/contents"+(i+1)+".txt");
+        	BufferedWriter bw = new BufferedWriter(new FileWriter(wordFile));
+        	bw.write(stringOfWords[i]);
+        	bw.close();
+        }
+        return corpusDirPath;
+	}
+
+
 }
