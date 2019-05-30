a78ba0e16a4a5f264be3768b355ec6151733f7f5
benoitfarley <benoit.farley@gmail.com>
2019-05-30 16:02:25 -0400

    wordsContainingMorpheme() now throws an Exception. The results now contains the frequencies of the words.

--- java/iutools-core/src/main/java/ca/pirurvik/iutools/MorphemeExtractor.java
+++ java/iutools-core/src/main/java/ca/pirurvik/iutools/MorphemeExtractor.java
@@ -1,8 +1,5 @@
 package ca.pirurvik.iutools;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,56 +10,49 @@ import java.util.regex.Pattern;
 
 import org.apache.log4j.Logger;
 
-import com.google.gson.Gson;
+import ca.nrc.datastructure.Pair;
+import ca.nrc.datastructure.trie.Trie;
 
 public class MorphemeExtractor {
 	
-	protected String dictionary = null;
+	protected String wordSegmentations = null;
+	protected CompiledCorpus corpus = null;
 	
 	public MorphemeExtractor() {
 	}
 	
-	public void useDictionary(File file) throws IOException {
-		if ( !file.exists() )
-			throw new IOException("The file "+file+" does not exist.");
-		BufferedReader fileReader = new BufferedReader(new FileReader(file));
-		dictionary = fileReader.readLine();
-		fileReader.close();
+	public void useCorpus(CompiledCorpus _corpus) throws IOException {
+		corpus = _corpus;
+		wordSegmentations = _corpus.getWordSegmentations();
 	}
 	
-	public void useDictionary(CompiledCorpus corpus) throws IOException {
-		dictionary = corpus.getWordSegmentations();
-	}
-	
-	public void setDictionary(String _dictionary) {
-		this.dictionary = _dictionary;
-	}
-	
-	public String getDictionary() {
-		return this.dictionary;
-	}
-	
-	public List<Words> wordsContainingMorpheme(String morpheme) {
+	public List<Words> wordsContainingMorpheme(String morpheme) throws Exception {
 		Logger logger = Logger.getLogger("MorphemeExtractor.wordsContainingMorpheme");
 		logger.debug("morpheme= "+morpheme);
 		List<Words> words = new ArrayList<Words>();
-		HashMap<String,List<String>> wordsForMorphemes = new HashMap<String,List<String>>();
-		if (dictionary==null)
-			return words;
-		String regexp = ",,([^:]+?):[^:]*?\\{("+morpheme+"/.+?)\\}[^:,]*?";
+		HashMap<String,List<Pair<String,Long>>> wordsForMorphemes = new HashMap<String,List<Pair<String,Long>>>();
+		if (wordSegmentations==null)
+			throw new Exception("The word extractor has not been defined a compiled corpus.");
+		Trie corpusTrie = corpus.getTrie();
+		String regexp = ",([^:,]+?):([^:]*?\\{("+morpheme+"/.+?)\\}[^:,]*?),";
 		Pattern p = Pattern.compile(regexp);
-		Matcher m = p.matcher(this.dictionary);
+		Matcher m = p.matcher(this.wordSegmentations);
 		while (m.find()) {
 			String word = m.group(1);
-			String morphemeWithId = m.group(2);
-			List<String> wordsForMorpheme;
+			String segments = m.group(2);
+			String morphemeWithId = m.group(3);
+			List<Pair<String,Long>> wordsForMorpheme;
 			if (wordsForMorphemes.containsKey(morphemeWithId)) {
 				wordsForMorpheme = wordsForMorphemes.get(morphemeWithId);
 			}
 			else {
-				wordsForMorpheme = new ArrayList<String>();
+				wordsForMorpheme = new ArrayList<Pair<String,Long>>();
 			}
-			wordsForMorpheme.add(word);
+			segments += " \\";
+			String segmentsPlusSpaces = segments.replaceAll("\\}\\{", "} {");
+			long freq = corpusTrie.getFrequency(segmentsPlusSpaces.split(" "));
+			logger.debug(word+": "+freq+" --- "+segmentsPlusSpaces);
+			wordsForMorpheme.add(new Pair<String,Long>(word,new Long(freq)));
 			wordsForMorphemes.put(morphemeWithId,wordsForMorpheme);
 		}
 		Iterator<String> it = wordsForMorphemes.keySet().iterator();
@@ -76,12 +66,13 @@ public class MorphemeExtractor {
 	public class Words {
 		
 		public String morphemeWithId;
-		public List<String> words;
+		public List<Pair<String,Long>> words;
 		
-		public Words(String _morphemeWithId, List<String> _words) {
+		public Words(String _morphemeWithId, List<Pair<String,Long>> _words) {
 			this.morphemeWithId = _morphemeWithId;
 			this.words = _words;
 		}
 	}
 
 }
+
