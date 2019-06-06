08adca53474c1ffef2164059a661d42ace24d124
desiletsa <alain.desilets@nrc-cnrc.gc.ca>
2019-06-05 20:53:47 -0400

    Prevent CompiledCorpusRegistry from generating two copies of default corpus

--- java/iutools-core/src/main/java/ca/pirurvik/iutools/CompiledCorpusRegistry.java
+++ java/iutools-core/src/main/java/ca/pirurvik/iutools/CompiledCorpusRegistry.java
@@ -12,72 +12,51 @@ import ca.nrc.config.ConfigException;
 public class CompiledCorpusRegistry {
 	
 	private static Map<String,CompiledCorpus> corpusCache = new HashMap<String,CompiledCorpus>();
-	private static Map<String,String> registry = new HashMap<String,String>();
+	private static Map<String,File> registry = new HashMap<String,File>();
+	public static String defaultCorpusName = "Hansard1999-2002";
 	
 	static {
 		try {
 			String Hansard19992002_compilationFilePath = IUConfig.getIUDataPath("data/tries/trie_compilation-HANSARD-1999-2002---single-form-in-terminals.json");
-			registry.put("Hansard1999-2002", Hansard19992002_compilationFilePath);
-			registry.put("default", Hansard19992002_compilationFilePath);
+			registry.put("Hansard1999-2002", new File(Hansard19992002_compilationFilePath));
 		} catch (ConfigException e) {
+			throw new ExceptionInInitializerError(e);
 		}
 	}
 	
+	public static void registerCorpus(String corpusName, File jsonFile) throws CompiledCorpusRegistryException {
+		if (registry.containsKey(corpusName) 
+				&&  !registry.get(corpusName).equals(jsonFile) ) {
+				throw new CompiledCorpusRegistryException("The name '"+corpusName+"' is already associated with a different compilation file.");
+		} else {
+			registry.put(corpusName, jsonFile);
+		}
+	}
+	
+	
 	@JsonIgnore
 	public static CompiledCorpus getCorpus() throws CompiledCorpusRegistryException {
-		return getCorpus("default");
+		return getCorpus(defaultCorpusName);
 	}
 
 	@JsonIgnore
 	public static CompiledCorpus getCorpus(String corpusName) throws CompiledCorpusRegistryException {
-		if (corpusName==null)
-			corpusName = "default";
+		if (corpusName==null) corpusName = defaultCorpusName;
 		CompiledCorpus corpus = null;
 		if (corpusCache.containsKey(corpusName)) {
 			corpus = corpusCache.get(corpusName);
 		} else {
-			if (corpusName.startsWith("FILE=") || registry.containsKey(corpusName)) {
-				String filePath;
-				if (corpusName.startsWith("FILE=")) {
-					filePath = corpusName.replace("FILE=", "");
-				} else {
-					filePath = registry.get(corpusName);
-				}
-				corpus = makeCorpus(filePath);
-				corpusCache.put(corpusName, corpus);
-			} else {
+			if (!registry.containsKey(corpusName)) {
 				throw new CompiledCorpusRegistryException("Unknown corpus name: "+corpusName);
+			} else {
+				String jsonFilePath = registry.get(corpusName).toString();
+				corpus = makeCorpus(jsonFilePath);
+				corpusCache.put(corpusName, corpus);
 			}
 		}
 		
 		return corpus;
 	}
-	
-	@JsonIgnore
-	public static CompiledCorpus getCorpus(File corpusJsonFPath) throws CompiledCorpusRegistryException {
-		String corpusName = "FILE="+corpusJsonFPath.toString();
-		return getCorpus(corpusName);
-	}
-	
-
-	public static void addCorpus(String corpusName, String compilationFilePath) throws CompiledCorpusRegistryException {
-		addCorpus(corpusName,compilationFilePath,false);
-	}
-	public static void addCorpus(String corpusName, String compilationFilePath, boolean makeItDefault) throws CompiledCorpusRegistryException {
-		if (registry.containsKey(corpusName)) {
-			if ( !registry.get(corpusName).equals(compilationFilePath) ) {
-				throw new CompiledCorpusRegistryException("The name '"+corpusName+"' is already associated with a different compilation file.");
-			}
-		} else {
-			registry.put(corpusName, compilationFilePath);
-		}
-		if (makeItDefault) {
-			registry.put("default", compilationFilePath);
-		}
-	}
-	
-
-	
 	
 	private static CompiledCorpus makeCorpus(String corpusJsonFPath) throws CompiledCorpusRegistryException  {
 		CompiledCorpus corpus = null;
@@ -87,7 +66,6 @@ public class CompiledCorpusRegistry {
 		}
 		try {
 			corpus = CompiledCorpus.createFromJson(corpusJsonFPath);
-			
 		} catch (Exception e) {
 			throw new CompiledCorpusRegistryException("Could not read compiled corpus from file: "+corpusJsonFPath, e);
 		}
@@ -95,7 +73,7 @@ public class CompiledCorpusRegistry {
 		return corpus;
 	}
 	
-	public static Map<String,String> getRegistry() {
+	public static Map<String,File> getRegistry() {
 		return registry;
 	}
 	public static Map<String,CompiledCorpus> getCorpusCache() {
--- java/iutools-core/src/test/java/ca/pirurvik/iutools/CompiledCorpusRegistryTest.java
+++ java/iutools-core/src/test/java/ca/pirurvik/iutools/CompiledCorpusRegistryTest.java
@@ -6,15 +6,66 @@ import static org.junit.Assert.assertTrue;
 import java.io.File;
 import java.util.Map;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import ca.nrc.datastructure.trie.Trie;
 import ca.nrc.datastructure.trie.TrieNode;
 
 public class CompiledCorpusRegistryTest {
+	
+	File jsonFile = null;
+	
+	@Before
+	public void setUp() throws Exception {
+		String[] words = new String[] {"nunami","iglumik"};
+		jsonFile = CompiledCorpusTest.compileToFile(words);
+	}
+	
+	//////////////////////////////
+	// DOCUMENTATION TESTS
+	//////////////////////////////
+	
+	@Test
+	public void test__CompiledCorpusRegistry__Synopsis() throws Exception {
+		//
+		// Use this class to acquire instances of CompiledCorpus
+		//
+		// For example, to get the "default" corpus, do this:
+		//
+		CompiledCorpus corpus = CompiledCorpusRegistry.getCorpus();
+		
+		//
+		// If you want to get a specific corpus, do this:
+		//
+		String corpusName = "Hansard1999-2002";
+		corpus = CompiledCorpusRegistry.getCorpus(corpusName);
+		
+		//
+		// The registry comes with some pre-packaged corpora.
+		// You can however register your own corpora in it.
+		// 
+		// Assuming that file jsonFile contains a JSON serialization of 
+		// a CompiledCorpus object, then you can add a new corpus as 
+		// follows
+		//
+		corpusName = "myCorpus";
+		CompiledCorpusRegistry.registerCorpus(corpusName, jsonFile);
+		
+		// 
+		// Once you have registered your own corpus, you can get an
+		// instance of it as usual
+		//
+		corpus = corpus = CompiledCorpusRegistry.getCorpus(corpusName);
+	}
+	
+	
+	//////////////////////////////
+	// VERIFICATION TESTS
+	//////////////////////////////
 
 	@Test
-	public void test__getCorpus__No_argument_get_default() throws CompiledCorpusRegistryException {
+	public void test__getCorpus__No_argument__Returns_default_corpus() throws CompiledCorpusRegistryException {
 		CompiledCorpus corpus = CompiledCorpusRegistry.getCorpus();
 		Trie trie = corpus.getTrie();
 		TrieNode[] ammaTerminals = trie.getAllTerminals(new String[] {"{amma/1c}"});
@@ -22,26 +73,26 @@ public class CompiledCorpusRegistryTest {
 		assertTrue("Incorrect number of terminals for amma/1c;\nexpected more than 0",got>0);
 	}
 	
-	@Test
-	public void test__getCorpus__get_from_file_path() throws Exception {
-		String[] words = new String[] {"nunami","iglumik"};
-		File corpusFile = CompiledCorpusTest.compileToFile(words);
-		corpusFile.deleteOnExit();
-		CompiledCorpus corpus = CompiledCorpusRegistry.getCorpus(corpusFile);
-		Trie trie = corpus.getTrie();
-		TrieNode[] nunaTerminals = trie.getAllTerminals(new String[] {"{nuna/1n}"});
-		int got = nunaTerminals.length;
-		assertTrue("Incorrect number of terminals for nuna/1n;\nexpected more than 0",got>0);
-		
-		Map<String,CompiledCorpus> corpusCache = CompiledCorpusRegistry.getCorpusCache();
-		assertTrue("The registry does not contain the corpus specified by the file path.",corpusCache.containsKey("FILE="+corpusFile));
-
-		CompiledCorpus corpus2words = CompiledCorpusRegistry.getCorpus(corpusFile);
-		Trie trie2words = corpus2words.getTrie();
-		TrieNode[] igluTerminals = trie2words.getAllTerminals(new String[] {"{iglu/1n}"});
-		got = igluTerminals.length;
-		assertTrue("Incorrect number of terminals for iglu/1n;\nexpected more than 0",got>0);
-	}
+//	@Test
+//	public void test__getCorpus__get_from_file_path() throws Exception {
+//		String[] words = new String[] {"nunami","iglumik"};
+//		File corpusFile = CompiledCorpusTest.compileToFile(words);
+//		corpusFile.deleteOnExit();
+//		CompiledCorpus corpus = CompiledCorpusRegistry.getCorpus(corpusFile);
+//		Trie trie = corpus.getTrie();
+//		TrieNode[] nunaTerminals = trie.getAllTerminals(new String[] {"{nuna/1n}"});
+//		int got = nunaTerminals.length;
+//		assertTrue("Incorrect number of terminals for nuna/1n;\nexpected more than 0",got>0);
+//		
+//		Map<String,CompiledCorpus> corpusCache = CompiledCorpusRegistry.getCorpusCache();
+//		assertTrue("The registry does not contain the corpus specified by the file path.",corpusCache.containsKey("FILE="+corpusFile));
+//
+//		CompiledCorpus corpus2words = CompiledCorpusRegistry.getCorpus(corpusFile);
+//		Trie trie2words = corpus2words.getTrie();
+//		TrieNode[] igluTerminals = trie2words.getAllTerminals(new String[] {"{iglu/1n}"});
+//		got = igluTerminals.length;
+//		assertTrue("Incorrect number of terminals for iglu/1n;\nexpected more than 0",got>0);
+//	}
 	
 	@Test
 	public void test__getCorpus__get_from_corpus_name_statically_initialized() throws CompiledCorpusRegistryException {
@@ -64,11 +115,11 @@ public class CompiledCorpusRegistryTest {
 	}
 	
 	@Test
-	public void test__getCorpus__get_from_added_corpus() throws Exception {
+	public void test__getCorpus__get_from_custom_registered_corpus() throws Exception {
 		String[] words = new String[] {"nunami","iglumik"};
 		File corpusFile = CompiledCorpusTest.compileToFile(words);
 		corpusFile.deleteOnExit();
-		CompiledCorpusRegistry.addCorpus("2words", corpusFile.getAbsolutePath());
+		CompiledCorpusRegistry.registerCorpus("2words", corpusFile);
 		CompiledCorpus corpus = CompiledCorpusRegistry.getCorpus("2words");
 		Trie trie = corpus.getTrie();
 		TrieNode[] nunaTerminals = trie.getAllTerminals(new String[] {"{nuna/1n}"});
@@ -77,23 +128,16 @@ public class CompiledCorpusRegistryTest {
 	}
 	
 	@Test
-	public void test__addCorpus__already_defined() throws CompiledCorpusRegistryException {
-		CompiledCorpusRegistry.addCorpus("XYZ", "file_A");
+	public void test__registerCorpus__already_defined() throws CompiledCorpusRegistryException {
+		CompiledCorpusRegistry.registerCorpus("XYZ", new File("file_A"));
 		
 		boolean allreadydefined = false;
 		try {
-			CompiledCorpusRegistry.addCorpus("XYZ", "file_B");
+			CompiledCorpusRegistry.registerCorpus("XYZ", new File("file_B"));
 		} catch (CompiledCorpusRegistryException e) {
 			allreadydefined = e.getMessage().contains("is already associated");
 		}
 		assertTrue("The exception 'is already associated' should have been thrown.",allreadydefined);
-	}
-	
-	@Test
-	public void test__addCorpus__make_it_default() throws CompiledCorpusRegistryException {
-		CompiledCorpusRegistry.addCorpus("XYZ", "file_A", true);
-		Map<String,String> registry = CompiledCorpusRegistry.getRegistry();
-		assertEquals("The added corpus has not been set as the default corpus.","file_A",registry.get("default"));
 	}
 	
 
