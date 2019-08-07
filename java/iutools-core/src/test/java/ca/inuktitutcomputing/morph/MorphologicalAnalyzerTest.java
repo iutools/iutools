c5479732d7d2df0c15012d64642414f4a2419d7f
benoitfarley <benoit.farley@gmail.com>
2019-08-07 11:10:54 -0400

    added tests

--- java/iutools-core/src/test/java/ca/inuktitutcomputing/morph/MorphologicalAnalyzerTest.java
+++ java/iutools-core/src/test/java/ca/inuktitutcomputing/morph/MorphologicalAnalyzerTest.java
@@ -84,4 +84,135 @@ public class MorphologicalAnalyzerTest {
 		}
 	}
 
+	@Test
+	public void test__decomposeWord__sivuliuqtii() throws Exception  {
+		MorphologicalAnalyzer analyzer = new MorphologicalAnalyzer();
+		String word = "sivuliuqtii";
+		try {
+			analyzer.disactivateTimeout();;
+			Decomposition[] decs = analyzer.decomposeWord(word);
+			Assert.assertTrue(decs.length!=0);
+			for (int i=0; i<decs.length; i++) {
+				System.out.println(decs[i].toStr2());
+			}
+		} catch(Exception e) {
+			throw e;
+		}
+	}
+
+	@Test
+	public void test__decomposeWord__ammalu() throws Exception  {
+		MorphologicalAnalyzer analyzer = new MorphologicalAnalyzer();
+		String word = "ammalu";
+		try {
+			analyzer.disactivateTimeout();;
+			Decomposition[] decs = analyzer.decomposeWord(word);
+			Assert.assertTrue(decs.length!=0);
+			for (int i=0; i<decs.length; i++) {
+				System.out.println(decs[i].toStr2());
+			}
+		} catch(Exception e) {
+			throw e;
+		}
+	}
+
+		
+	/*
+	 * Ce test vérifie que les analyses qui contiennent des séquences de
+	 * morphèmes pour lesquelles il existe un morphème combiné sont supprimées.
+	 * Ex. : la première analyse est supprimée puisque dans la seconde, il y a
+	 *       le morphème juksaq qui est la combinaison de juq et ksaq.
+	 * {apiq:apiq/1v}{suq:suq/1vv}{ta:jaq/1vn}{u:u/1nv}{ju:juq/1vn}{ksaq:ksaq/1nn}
+	 * {apiq:apiq/1v}{suq:suq/1vv}{ta:jaq/1vn}{u:u/1nv}{juksaq:juksaq/1vn}
+	 */
+	@Test
+	public void test__decomposeWord__apiqsuqtaujuksaq() throws Exception  {
+		MorphologicalAnalyzer analyzer = new MorphologicalAnalyzer();
+		String word = "apiqsuqtaujuksaq";
+
+		try {
+			analyzer.disactivateTimeout();;
+			Decomposition[] decs = analyzer.decomposeWord(word);
+			Assert.assertTrue(decs.length!=0);
+			for (int i=0; i<decs.length; i++) {
+				Assert.assertTrue(!decs[i].toStr2().contains("{ju:juq/1vn}{ksaq:ksaq/1nn}"));
+			}
+		} catch(Exception e) {
+			throw e;
+		}
+	}
+
+
+	@Test
+	public void test__decomposeWord__immagaa() throws Exception  {
+		MorphologicalAnalyzer analyzer = new MorphologicalAnalyzer();
+		String word = "immagaa";
+
+		try {
+			analyzer.disactivateTimeout();;
+			Decomposition[] decs = analyzer.decomposeWord(word);
+			Assert.assertTrue(decs.length==0);
+			for (int i=0; i<decs.length; i++) {
+				System.out.println(decs[i].toStr2());
+			}
+		} catch(Exception e) {
+			throw e;
+		}
+	}
+
+
+
+
+	@Test
+	public void test__decomposeWord__avunngaqtuq() throws Exception  {
+		MorphologicalAnalyzer analyzer = new MorphologicalAnalyzer();
+		String word = "avunngaqtuq";
+
+		try {
+			analyzer.disactivateTimeout();;
+			Decomposition[] decs = analyzer.decomposeWord(word);
+			Assert.assertTrue(decs.length!=0);
+			for (int i=0; i<decs.length; i++) {
+				System.out.println(decs[i].toStr2());
+			}
+			
+			word = "avunngaujjijuq";
+			decs = analyzer.decomposeWord(word);
+			Assert.assertTrue(decs.length!=0);
+			for (int i=0; i<decs.length; i++) {
+				System.out.println(decs[i].toStr2());
+			}
+			
+			word = "avunngautijuq";
+			decs = analyzer.decomposeWord(word);
+			Assert.assertTrue(decs.length!=0);
+			for (int i=0; i<decs.length; i++) {
+				System.out.println(decs[i].toStr2());
+			}
+		} catch(Exception e) {
+			throw e;
+		}
+	}
+
+
+	@Test
+	public void test__decomposeWord__atuagaq() throws Exception  {
+		MorphologicalAnalyzer analyzer = new MorphologicalAnalyzer();
+		String word = "atuagaq";
+
+		try {
+			analyzer.disactivateTimeout();;
+			Decomposition[] decs = analyzer.decomposeWord(word);
+			Assert.assertTrue(decs.length!=0);
+			for (int i=0; i<decs.length; i++) {
+				System.out.println(decs[i].toStr2());
+			}
+		} catch(Exception e) {
+			throw e;
+		}
+	}
+
+
+	
+
 }
