440e7561c6bc1f1e6dff9b6f3da9c46ab34ce740
benoitfarley <benoit.farley@gmail.com>
2019-09-09 15:48:12 -0400

    Added class to handle .docx documents

---
+++ java/iutools-core/src/main/java/ca/inuktitutcomputing/documents/NRC_DOCXDocumentException.java
@@ -0,0 +1,11 @@
+package ca.inuktitutcomputing.documents;
+
+public class NRC_DOCXDocumentException extends Exception {
+	
+	public NRC_DOCXDocumentException(Exception e) { super(e); }
+
+	public NRC_DOCXDocumentException(String mess) { super (mess); }
+
+	public NRC_DOCXDocumentException(String mess, Exception e) { super(mess, e); }
+
+}
