52e375d3d01073d0b6f3ebdc5a48ef683d39f9bc
desiletsa <alain.desilets@nrc-cnrc.gc.ca>
2019-09-11 14:51:39 -0400

    Created small executable for downloading URL

---
+++ java/iutools-core/src/main/java/ca/inuktitutcomputing/core/console/URLDownloader.java
@@ -0,0 +1,19 @@
+package ca.inuktitutcomputing.core.console;
+
+import ca.nrc.data.harvesting.PageHarvester;
+import ca.nrc.data.harvesting.PageHarvesterException;
+
+public class URLDownloader {
+
+	public static void main(String[] args) throws PageHarvesterException {
+		PageHarvester harvester = new PageHarvester();
+		String url = args[0];
+		System.out.println("Harvesting content of url: "+url);
+		harvester.harvestSinglePage(url);
+		String text = harvester.getText();
+		System.out.println("Text is:\n\n"+text);
+		String html = harvester.getHtml();
+		System.out.println("\n============\nhtml is:\n\n"+html);
+	}
+
+}
