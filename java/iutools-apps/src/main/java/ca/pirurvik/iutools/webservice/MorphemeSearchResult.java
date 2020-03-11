5e6cd293aea07697043f5fffc20b52ef91a61e61
benoitfarley <benoit.farley@gmail.com>
2020-03-11 12:51:22 -0400

    Changed name of attribute from wordFreqs to wordScores

--- java/iutools-apps/src/main/java/ca/pirurvik/iutools/webservice/MorphemeSearchResult.java
+++ java/iutools-apps/src/main/java/ca/pirurvik/iutools/webservice/MorphemeSearchResult.java
@@ -6,15 +6,15 @@ import java.util.List;
 class MorphemeSearchResult {
 	public String  meaning;
 	public List<String> words;
-	public List<Long> wordFrequencies;
+	public List<Double> wordScores;
 	
 	public MorphemeSearchResult() {
 	};
 	
-	public MorphemeSearchResult(String _meaning, List<String> _words, List<Long> _wordFrequencies) {
+	public MorphemeSearchResult(String _meaning, List<String> _words, List<Double> _wordScores) {
 		this.meaning = _meaning;
 		this.words = _words;
-		this.wordFrequencies = _wordFrequencies;
+		this.wordScores = _wordScores;
 	}
 }
 
