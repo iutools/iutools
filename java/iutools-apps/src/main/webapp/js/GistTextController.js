c8d3f6f93ab8948426c806f0f0c9c33ff4f94d46
desiletsa <alain.desilets@nrc-cnrc.gc.ca>
2020-04-06 11:10:34 -0400

    [REF] Getting rid of old stuff in Gist UI

--- java/iutools-apps/src/main/webapp/js/GistTextController.js
+++ java/iutools-apps/src/main/webapp/js/GistTextController.js
@@ -6,274 +6,169 @@ class GistTextController extends WidgetController {
 
 	constructor(config) {
 		super(config);
+		this.busy = false;
 		this.wordGistController = new WordGistController(config);
 	} 
 	
 	// Setup handler methods for different HTML elements specified in the config.
 	attachHtmlElements() {
-		this.setEventHandler("btnPaste", "click", this.onButtonPaste)
-		this.setEventHandler("divGistIconizer", "click", this.iconizeDivGist);
-		this.setEventHandler("divGistIconized", "click", this.deiconizeDivGist);
+		this.setEventHandler("btnGist", "click", this.onGistText)
 	}
 	
-	onButtonPaste() {
-        navigator.clipboard.readText().then(
-        	clipText => this.prepareTextForGisting(clipText));
+	onGistText() {
+		var textOrUrl = this.elementForProp('txtUrlOrText').val();
+		this.prepareContent(textOrUrl);
     }
 	
-	prepareTextForGisting(text) {
-		var divText = this.elementForProp('divText');
-		var textWithLinks = this.addLinksToText(text);
-		divText.html(textWithLinks);
-		this.attachListenersToIUWords(this);
-	}
-	
-	attachListenersToIUWords(controller) {
-		var anchorsWords = document.querySelectorAll('.iuword');
-	    for (var ipn=0; ipn<anchorsWords.length; ipn++) {
-	    	anchorsWords[ipn].addEventListener(
-		    		  'click', 
-		    		  controller.onCLickIUWord
-		    		  );
-	    }
-	}
-
-	
-	onCLickIUWord() {
-		var clickedWord = $(this).text();
-		var jsonRequestData = "{\"word\":\""+clickedWord+"\"}"
-		var controller = gistTextController;
-		var fctSuccess = 
-				function(resp) {
-					controller.successGetGistCallback.call(controller, resp);
-				};
-		var fctFailure = 
-				function(resp) {
-					controller.failureGetGistCallback.call(controller, resp);
-				};
-		$.ajax({
-			method: 'POST',
-			url: 'srv/gisttext',
-			data: jsonRequestData,
-			dataType: 'json',
-			async: true,
-			success: fctSuccess,
-			error: fctFailure
-		});
-	}
-	
-	addLinksToText(text) {
-		var output = "";
-		var tokens = text.split(" ");
-		// TODO: envoyer un appel AJAX au code JAVA IUTokenizer
-		for (var itok=0; itok<tokens.length; itok++) {
-			if (this.allInuktitutCharacters(tokens[itok]))
-				output += "<span class='iuword'>"+tokens[itok]+"</span>";
-			else
-				output += tokens[itok];
-			output += " ";
-		}
-		return output;
-	}
-	
-	allInuktitutCharacters(text) {
-		var res = true;
-		var iuchars = ["a","i","u","g","j","k","l","m","n","p","q","r","s","t","v","&"];
-		for (var ich=0; ich<text.length; ich++)
-			if ( !iuchars.includes(text.charAt(ich)) ) {
-				res = false;
-				break;
-			}
-		return res;
-	}
-
-	iconizeDivGist() {
-		var divGist = this.elementForProp("divGist");
-		divGist.hide();
-		var divGistIconized = this.elementForProp("divGistIconized");
-		divGistIconized.show();
-	}
-	
-	deiconizeDivGist() {
-		var divGist = this.elementForProp("divGist");
-		divGist.show();
-		var divGistIconized = this.elementForProp("divGistIconized");
-		divGistIconized.hide();
-	}
-	
-//	clearResults() {
-//		this.elementForProp('divError').empty();
-//		this.elementForProp('divResults').empty();
-//	}
-//	
-//	invokeGetService(jsonRequestData, _successCbk, _failureCbk) {
-//		this.invokeSearchService(jsonRequestData, _successCbk, _failureCbk);
-//	}
-//	
-//	invokeSearchService(jsonRequestData, _successCbk, _failureCbk) {
-//			var tracer = new Tracer('GistTextController.invokeSearchService', true);
-//			tracer.trace("_successCbk="+_successCbk+", jsonRequestData="+JSON.stringify(jsonRequestData));
-//			this.busy = true;
-//			var controller = this;
-//			var fctSuccess = 
-//					function(resp) {
-//						_successCbk.call(controller, resp);
-//					};
-//			var fctFailure = 
-//					function(resp) {
-//						_failureCbk.call(controller, resp);
-//					};
-//					
-//			// this line is for development only, allowing to present results without calling Bing.
-//			//var jsonResp = this.mockSrvSearch();fctSuccess(jsonResp);
-//		
-//			$.ajax({
-//				method: 'POST',
-//				url: 'srv/gisttext',
-//				data: jsonRequestData,
-//				dataType: 'json',
-//				async: true,
-//		        success: fctSuccess,
-//		        error: fctFailure
-//			});
-//			
-//			tracer.trace('GistTextController.invokeSearchService', "exited");
-//	}
-//	
-//	validateQueryInput() {
-//		var isValid = true;
-//		var query = this.elementForProp("inpWord").val();
-//		if (query == null || query === "") {
-//			isValid = false;
-//			this.error("You need to enter something in the word field");
-//		}
-//		return isValid;
-//	}
-//
-	successGetGistCallback(resp) {
-		var tracer = new Tracer('GistTextController.successGetCallback', true);
-		tracer.trace("resp="+JSON.stringify(resp));
+	prepareContent(textOrUrl) {
+		var inputs = {
+			textOrUrl: textOrUrl
+		};
+		var json_inputs = JSON.stringify(inputs);
 		
-		if (resp.errorMessage != null) {
-			this.failureGetCallback(resp);
-		} else {
-			this.setGetResults(resp);	
-		}
-//		this.setGetBusy(false);
-		tracer.trace("exited");
+		this.clearResults();
+		this.setBusy(true);
+		this.invokeWebService('srv/gist/preparecontent/', json_inputs, 
+				this.prepareContentSuccessCallback, 
+				this.prepareContentFailureCallback);
 	}
 	
-	failureGetGistCallback(resp) {
+	prepareContentSuccessCallback(resp) {
+		console.log('resp= '+JSON.stringify(resp));
+		if (resp.errorMessage != null) {
+			this.prepareContentFailureCallback(resp);
+		} else {
+			console.log("** GistTextNEWController.prepareContentSuccessCallback: successfully prepared text to: "+
+				JSON.stringify(resp));
+			this.setBusy(false);
+			this.displayTextGist(resp);
+		}
+	}
+
+	prepareContentFailureCallback(resp) {
 		if (! resp.hasOwnProperty("errorMessage")) {
 			// Error condition comes from tomcat itself, not from our servlet
 			resp.errorMessage = 
 				"Server generated a "+resp.status+" error:\n\n" +
 				resp.responseText;
 		}				
-		gistTextController.error(resp.errorMessage);
+		this.error(resp.errorMessage);
+		this.setBusy(false);
+	}
+	
+	displayTextGist(response) {
+		var iuSentences = response.iuSentences;
+		var enSentences = response.enSentences;
+		if (enSentences != null) {
+			this.displayBilingualGist(iuSentences, enSentences);
+		} else {
+			this.displayUnilingualGist(iuSentences);
+		}
+	}
+	
+	displayUnilingualGist(iuSentences) {
+		var html = "";
+		for (var sentNum=0; sentNum < iuSentences.length; sentNum++) {
+			var sent = iuSentences[sentNum];
+			for (var tokenNum=0; tokenNum < sent.length; tokenNum++) {
+				var token = sent[tokenNum];
+				if (IUUtils.isInuktut(token)) {
+					html += "<a class=\"iu-word\">"+token+"</a>";
+				} else {
+					html += token;
+				}
+			}
+		}
+		this.displayTextWithClickableWords(html);
+	}	
+	
+	displayBilingualGist(iuSentences, enSentences) {
+		var html = "";
+		var maxSents = Math.max(iuSentences.length, enSentences.length);
+		if (maxSents > 0) {
+			html += 	
+				"<table id=\"tbl-alignments\" class=\"alignments\" style=\"table-layout: fixed; width: 100%\">\n"+
+				"<tr><th>Inuktitut</th><th>English</th></tr>\n";
+			
+			for (var sentNum=0; sentNum < maxSents; sentNum++) {
+				// Create new row for an alignment
+				html += "<tr>\n"
+				html = this.htmlAddAlignmentOneSide(html, sentNum, iuSentences);
+				html = this.htmlAddAlignmentOneSide(html, sentNum, enSentences);
+				// Close the current alignment row
+				html += "<tr>\n"
+			}
+			html += "</table>";
+			this.displayTextWithClickableWords(html);
+		}		
+	}
+	
+	htmlAddAlignmentOneSide(html, sentNum, sentences) {
+		html += "<td>"
+		var sent = [];
+		if (sentences.length > sentNum) {
+			sent = sentences[sentNum];
+		}
+		for (var ii=0; ii < sent.length; ii++) {
+			var token = sent[ii];
+			if (IUUtils.isInuktut(token)) {
+				html += '<a class="iu-word">'+token+"</a>";
+			} else {
+				html += token;
+			}
+		}
+		html += "</td>"
+			
+		return html;
+	}
+	
+	displayTextWithClickableWords(html) {
+		html = "<h2>Click on words to see their gist</h2>\n"+html;
+		var div_results = this.elementForProp("divGistTextResults");
+		div_results.html(html);
+		this.attachListenersToIUWords(this)
+	}	
+
+	attachListenersToIUWords(controller) {
+		var anchorWords = $(document).find('.iu-word');
+	    for (var ipn=0; ipn<anchorWords.length; ipn++) {
+	    	this.setEventHandler(anchorWords.eq(ipn), "click", this.onCLickIUWord);	   
+	    }		
+	}
+	
+	clearResults() {
+		var div_results = this.elementForProp("divGistTextResults");
+		div_results.html("");		
+	}
+	
+	onCLickIUWord(evt) {
+		var element = evt.target;
+		var iuWord = $(element).text();
+		this.wordGistController.gistWord(iuWord);				
 	}
 
 	error(err) {
 		this.elementForProp('divError').html(err);
 		this.elementForProp('divError').show();	 
 	}
-
-	setGetResults(results) {
-		var tracer = new Tracer('GistTextController.setGetResults', true);
-		var jsonResults = JSON.stringify(results);
-		var divGist = gistTextController.elementForProp("divGist");
-		var divGistContents = gistTextController.elementForProp("divGistContents");
-		divGistContents.empty();
-		var divGistWordHolder = gistTextController.elementForProp("divGistWordHolder");
-		divGistWordHolder.text("word");
-
-		var decompositions = results.decompositions;
-		tracer.trace("nb. decompositions="+decompositions.length);
-		var html = 'The word has the following decompositions: ';
-		html += '<div id="gist-decompositions">';
-		for (var idec=0; idec<decompositions.length; idec++) {
-			html += this.generateTableForDecomposition(decompositions[idec]);
-			var decomposition = decompositions[idec];
-		}
-		html += '</div>';
-		
-		var alignments = results.sentencePairs;
-		if (alignments.length != 0) {
-			html += "<div id='gist-alignments'>"+
-				"table id=\"tbl-alignments\" style=\"table-layout: fixed; width: 100%\" class=\"alignments\">\n"+
-				"<th>Inuktitut</th><th>English</th></tr>\n";
-			for (var ial=0; ial<Math.min(30,alignments.length); ial++) {
-				var inuktitutSentence = alignments[ial].sentences.iu;
-				var englishSentence = alignments[ial].sentences.en;
-				html += '<tr><td>'+inuktitutSentence+'</td><td>'+englishSentence+'</td></tr>';
-			}
-			html += '</table></div>';
+	
+	setBusy(flag) {
+		this.busy = flag;
+		if (flag) {
+			this.disableGistTextButton();	
+			this.showSpinningWheel('divMessage', "Gisting text...");
+			this.error("");
 		} else {
-			html += "This word was not found in the Hansard 1999-2002.";
+			this.enableGistTextButton();		
+			this.hideSpinningWheel('divMessage');
 		}
-		
-		divGistContents.html(html);
-
-		new RunWhen().domReady(function() {
-				divGist.show();
-		});
+	}	
+	
+	disableGistTextButton() {
+		this.elementForProp('btnGist').attr("disabled", true);
 	}
 	
-	generateTableForDecomposition(decomposition) {
-		var parts = decomposition.parts;
-		var meanings = decomposition.meanings;
-		var html = "<table>";
-		for (var ipart=0; ipart<parts.length; ipart++) {
-			html += "<tr"+(ipart==0?" class='root'":"")+">"
-				+"<td>"+parts[ipart].surface+"</td>"
-				+"<td>"+meanings[ipart]+"</td>"
-				+"</tr>";
-		}
-		html += "</table>";
-		return html;
-	}
-//		
-//	
-//	// ---------------------- Test Section ------------------------------ //
-//
-//	onTest() {
-//		this.invokeTestService({}, 
-//				this.testSuccessCallback, this.testFailureCallback);
-//	}
-//
-//	invokeTestService(jsonRequestData, _successCbk, _failureCbk) {
-//			var controller = this;
-//			var fctSuccess = 
-//					function(resp) {
-//						_successCbk.call(controller, resp);
-//					};
-//			var fctFailure = 
-//					function(resp) {
-//						_failureCbk.call(controller, resp);
-//					};
-//		
-//			$.ajax({
-//				method: 'POST',
-//				url: 'srv/hello',
-//				data: jsonRequestData,
-//				dataType: 'json',
-//				async: true,
-//		        success: fctSuccess,
-//		        error: fctFailure
-//			});
-//	}
-//	
-//	testSuccessCallback(resp) {
-//		var element = this.elementForProp("divTestResponse");
-//		element.empty();
-//		element.html(resp.message);
-//	}
-//	    
-//	testFailureCallback(resp) {
-//		var element = this.elementForProp("divTestResponse");
-//		element.empty();
-//		element.html("Server returned error, resp="+JSON.stringify(resp));
-//	}
-	
-
+	enableGistTextButton() {
+		this.elementForProp('btnGist').attr("disabled", false);
+	}	
 }
