0a1979aa2e16b0d643799a6adb9cbcc5f6c01190
desiletsa <alain.desilets@nrc-cnrc.gc.ca>
2020-10-09 09:07:26 -0400

    [REF] Making SpellChecker abstract with suclasses for ES and InMemory

--- iutools-core/src/test/java/ca/pirurvik/iutools/spellchecker/SpellChecker_InMemoryTest.java
+++ iutools-core/src/test/java/ca/pirurvik/iutools/spellchecker/SpellChecker_InMemoryTest.java
@@ -3,7 +3,6 @@ package ca.pirurvik.iutools.spellchecker;
 import org.junit.Ignore;
 
 // TODO-June2020: Activate this test and make it use an InMemory corpus
-@Ignore
 public class SpellChecker_InMemoryTest extends SpellCheckerTest {
 
 }
