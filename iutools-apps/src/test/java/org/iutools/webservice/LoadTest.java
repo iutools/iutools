bf70e157549ba73e03790ebc1c1c70f26681c2d0
desiletsa <alain.desilets@nrc-cnrc.gc.ca>
2020-12-24 06:41:05 -0500

    Running jmeter load tests from junit (started work)

--- iutools-apps/src/test/java/org/iutools/webservice/LoadTest.java
+++ iutools-apps/src/test/java/org/iutools/webservice/LoadTest.java
@@ -1,9 +1,9 @@
 package org.iutools.webservice;
 
-//import org.apache.jmeter.reporters.Summariser;
-//import org.apache.jmeter.save.SaveService;
-//import org.apache.jmeter.util.JMeterUtils;
-//import org.apache.jorphan.collections.HashTree;
+import org.apache.jmeter.reporters.Summariser;
+import org.apache.jmeter.save.SaveService;
+import org.apache.jmeter.util.JMeterUtils;
+import org.apache.jorphan.collections.HashTree;
 
 import java.io.File;
 
@@ -11,29 +11,29 @@ public class LoadTest {
 
 	public static void main(String[] args) throws Exception {
 
-//		// JMeter Engine
+		// JMeter Engine
 //		StandardJMeterEngine jmeter = new StandardJMeterEngine();
 //
-//		// Initialize Properties, logging, locale, etc.
-//		JMeterUtils.loadJMeterProperties("/tmp/jmeter/bin/jmeter.properties");
-//		JMeterUtils.setJMeterHome("/tmp/jmeter");
-//		JMeterUtils.initLogging();// you can comment this line out to see extra log messages of i.e. DEBUG level
-//		JMeterUtils.initLocale();
-//
-//		// Initialize JMeter SaveService
-//		SaveService.loadProperties();
-//
-//		// Load Test Plan
-//		HashTree testPlanTree = SaveService.loadTree(new File("/tmp/jmeter/bin/test.jmx"));
-//
-//		Summariser summer = null;
-//		String summariserName = JMeterUtils.getPropDefault("summariser.name", "summary");
-//		if (summariserName.length() > 0) {
-//			summer = new Summariser(summariserName);
-//		}
-//
-//
-//		// Store execution results into a .jtl file
+		// Initialize Properties, logging, locale, etc.
+		JMeterUtils.loadJMeterProperties("/tmp/jmeter/bin/jmeter.properties");
+		JMeterUtils.setJMeterHome("/tmp/jmeter");
+		JMeterUtils.initLogging();// you can comment this line out to see extra log messages of i.e. DEBUG level
+		JMeterUtils.initLocale();
+
+		// Initialize JMeter SaveService
+		SaveService.loadProperties();
+
+		// Load Test Plan
+		HashTree testPlanTree = SaveService.loadTree(new File("/tmp/jmeter/bin/test.jmx"));
+
+		Summariser summer = null;
+		String summariserName = JMeterUtils.getPropDefault("summariser.name", "summary");
+		if (summariserName.length() > 0) {
+			summer = new Summariser(summariserName);
+		}
+
+
+		// Store execution results into a .jtl file
 //		String logFile = "/tmp/jmeter/bin/test.jtl";
 //		ResultCollector logger = new ResultCollector(summer);
 //		logger.setFilename(logFile);
