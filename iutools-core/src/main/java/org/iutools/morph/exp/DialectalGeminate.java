38192c3fbf9cb46c6836af291b67bfefe5c4b1b6
benoitfarley <benoit.farley@gmail.com>
2021-01-21 10:09:31 -0500

    n/a

--- iutools-core/src/main/java/org/iutools/morph/exp/DialectalGeminate.java
+++ iutools-core/src/main/java/org/iutools/morph/exp/DialectalGeminate.java
@@ -7,23 +7,25 @@ public class DialectalGeminate {
 	
     private static String[][] groups = new String[][] {
         // C: g, j, k, l, m, n, ng, p, q, r, s, t, v
+
         // labC > CC : p(b), v, m
         { "bl", "ll" }, { "bj", "jj" }, { "bg", "gg" }, { "bv", "vv" },
         { "pl", "ll" }, { "pk", "kk" }, { "pg", "gg" }, { "pv", "vv" }, { "pq", "qq" },
         { "ps", "ts", "ss"},
         { "pt", "tt"},
         { "mng", "nng" }, {"mn", "nn"}, {"mp","pp"},
+
         // alvC > CC : t, s, &, l, n
-        { "tp", "pp" }, { "tk", "kk" }, { "tj", "jj" }, { "ts", "ss", "tt" }, 
-                {"t&", "ts", "tt"},
+        { "tp", "pp" }, { "tk", "kk" }, { "tj", "jj" }, { "ts", "ss", "tt" }, {"t&", "ts", "tt"},
         { "&&", "tt", "ts" }, 
         { "lv", "vv" },
-        {"nm","mm"},
+        { "nm","mm"},
+
         // velC > CC : k, g, ng
-        { "kt", "tt" }, { "ks", "ss", "ts" }, { "kp", "pp" }, { "kv", "vv" },
-                { "k&", "ss", "ts", "kt" },
+        { "kt", "tt" }, { "ks", "ss", "ts" }, { "kp", "pp" }, { "kv", "vv" }, { "k&", "ss", "ts", "kt" },
         { "gl", "ll" }, { "gv", "vv" }, 
         { "ngm", "mm" }, { "ngn", "nn" },
+
         // uvuC > CC : q, r
         { "qt", "tt", "rt" }, { "q&", "r&","qs", "qt" }, {"ql", "rl"}, {"qp", "rp"}, {"qs", "rs"},
         { "rq", "qq" },
