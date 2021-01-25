8242ca43b6e64c4b19c25efd61fa3b31168f7156
desiletsa <alain.desilets@nrc-cnrc.gc.ca>
2021-01-25 14:00:31 -0500

    Added a setUp thread group to jmeter test

---
+++ iutools-apps/src/test/java/org/iutools/webservice/InvokeAllEndpointsThroughServerTest.java
@@ -0,0 +1,47 @@
+package org.iutools.webservice;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.iutools.webservice.gist.GistWordInputs;
+import org.iutools.webservice.tokenize.GistPrepareContentInputs;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This test case invokes every end point multiple times through the
+ * server.
+ *
+ * The purpose of this is to ensure that threads for each end point have
+ * been created and initialise, and that future calls to the endpoint will
+ * not encur that any thread initialisation overhead.
+ *
+ * This in turn ensures that when we run the jmeter tests, the first few
+ * endpoint calls won't be slower than normal and cause performance assertion
+ * failures.
+ */
+public class InvokeAllEndpointsThroughServerTest {
+
+	@Test
+	public void invokeAllEndpoints() throws InterruptedException {
+		Map<String, ServiceInputs> callsToMake =
+		new HashMap<String, ServiceInputs>();
+		{
+			callsToMake.put("search", new SearchInputs("nunavut"));
+			callsToMake.put("spell", new SpellInputs("inukttttut"));
+			callsToMake.put("tokenize", new GistPrepareContentInputs("nunavut"));
+			callsToMake.put("occurrences", new OccurenceSearchInputs("nunavut", null, "20"));
+			callsToMake.put("gist/preparecontent", new GistPrepareContentInputs("nunavut"));
+			callsToMake.put("gist/gistword", new GistWordInputs("nunavut"));
+		}
+
+		for (int ii = 0; ii < 10; ii++) {
+			for (String srvPath : callsToMake.keySet()) {
+				ServiceInputs inputs = callsToMake.get(srvPath);
+			}
+			Thread.sleep(1*1000);
+		}
+
+		Thread.sleep(30*1000);
+	}
+}
