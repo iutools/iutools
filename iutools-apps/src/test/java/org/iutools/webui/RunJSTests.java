a2d86243def8174c8cf10b51544ba094ffd9f0a2
desiletsa <alain.desilets@nrc-cnrc.gc.ca>
2021-02-03 09:19:16 -0500

    Work in progress: RunJSTests class for running QUnit tests

---
+++ iutools-apps/src/test/java/org/iutools/webui/RunJSTests.java
@@ -0,0 +1,19 @@
+package org.iutools.webui;
+
+import ca.nrc.config.ConfigException;
+import ca.nrc.ui.web.testing.WebDriverFactory;
+import org.junit.Ignore;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Run all the client-side QUnit tests throug a browser.
+ */
+public class RunJSTests {
+
+	@Test @Ignore
+	public void test__RunJSTests() throws Exception  {
+		WebDriver driver = WebDriverFactory.getDriver();
+		driver.get("http://localhost:8080/iutools/allJSTests.html");
+	}
+}
