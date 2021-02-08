ba44cf4e44eeb05d2f44f6a4c4cfdd719c923812
desiletsa <alain.desilets@nrc-cnrc.gc.ca>
2021-02-08 14:25:40 -0500

    [ENH] Improvements to admin bash scripts for updating and redeploying iutools version

--- admin/lib/validate_JAVAUTILS_SOURCES.bash
+++ admin/lib/validate_JAVAUTILS_SOURCES.bash
@@ -1,6 +1,6 @@
 if [ -z ${JAVAUTILS_SOURCES} ]; then
   export JAVAUTILS_SOURCES=`realpath ${IUTOOLS_SOURCES}/../java-utils`
-  if [! -d $JAVAUTILS_SOURCES]; then
+  if [ ! -d $JAVAUTILS_SOURCES ]; then
     echo "JAVAUTILS_SRC environment variable not set. It should point to the root of the java-utils sources" 1>&2
     exit 1
   fi
---
+++ admin/update-all-and-redeploy.bash
@@ -0,0 +1,7 @@
+admin_path=`realpath $(dirname "$0")`
+
+# Makes the bash script to print out every command before it is executed except echo
+trap '[[ $BASH_COMMAND != echo* ]] && echo $BASH_COMMAND' DEBUG
+
+bash $admin_path/update-all.bash
+bash $admin_path/redeploy-webapps.bash
