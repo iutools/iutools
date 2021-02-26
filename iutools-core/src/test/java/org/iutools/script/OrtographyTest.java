6d4efb4d3d6b97bfe3570c2b79f3e2f519fa70dc
benoitfarley <benoit.farley@gmail.com>
2021-02-26 10:30:56 -0500

    Added tests

--- iutools-core/src/test/java/org/iutools/script/OrtographyTest.java
+++ iutools-core/src/test/java/org/iutools/script/OrtographyTest.java
@@ -22,4 +22,20 @@ public class OrtographyTest {
                 "Trimming did not produce expected results for word: "+word,
                 "nunavu", gotTrimmed);
     }
+
+    @Test
+    public void test__orthographyICI() {
+        String word = "nunavut";
+        String wordICI = Orthography.orthographyICI(word,true);
+        String expectedICI = "nunavut";
+        AssertString.assertStringEquals("",expectedICI,wordICI);
+    }
+
+    @Test
+    public void test__orthographyICILat() {
+        String word = "nunavut";
+        String wordICI = Orthography.orthographyICILat(word);
+        String expectedICI = "nunavut";
+        AssertString.assertStringEquals("",expectedICI,wordICI);
+    }
 }
