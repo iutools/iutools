e23ee64f4b20ba2e0c0e1734640bd23bf0128001
desiletsa <alain.desilets@nrc-cnrc.gc.ca>
2021-03-01 08:27:14 -0500

    [REF] Refactor the MorphRelsFinder performance test

---
+++ iutools-core/src/test/java/org/iutools/morphrelatives/WordOutcome.java
@@ -0,0 +1,95 @@
+package org.iutools.morphrelatives;
+
+import org.apache.commons.lang.ArrayUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Outcome (expected or actual) of running the MorphRelativesFinder on a word
+ */
+public class WordOutcome {
+	public String word = null;
+	public String[] relsProduced = new String[0];
+
+	public WordOutcome(String _word, String[] _relsProduced) {
+		init_WordOutcome(_word, _relsProduced);
+	}
+
+	private void init_WordOutcome(String _word, String[] _relsProduced) {
+		this.word = _word;
+		this.relsProduced = _relsProduced;
+	}
+
+	public List<String> correctRelatives(String[] goldStandardRels) {
+		List<String> correct = new ArrayList<String>();
+		for (String rel: relsProduced) {
+			if (ArrayUtils.contains(goldStandardRels, rel)) {
+				correct.add(rel);
+			}
+		}
+
+		return correct;
+	}
+
+	public List<String> incorrectRelatives(String[] goldStandardRels) {
+		List<String> incorrect = new ArrayList<String>();
+		for (String rel: relsProduced) {
+			if (!ArrayUtils.contains(goldStandardRels, rel)) {
+				incorrect.add(rel);
+			}
+		}
+		return incorrect;
+	}
+
+	public String diffsWithGoldStandard(String[] goldStandardRelatives) {
+		return diffsWithGoldStandard(goldStandardRelatives, "  ");
+	}
+
+	public String diffsWithGoldStandard(String[] goldStandardRelatives, String padding) {
+		String toS = "";
+		toS += padding+"Word: "+word+"\n";
+
+		toS += padding+"Precision: "+precision(goldStandardRelatives)+"\n";
+		toS += padding+"Recal: "+recall(goldStandardRelatives)+"\n";
+
+		toS += padding+"Relatives produced (** = correct): "+"\n";
+
+		List<String> goodRels = correctRelatives(goldStandardRelatives);
+		for (String goodRel: goodRels) {
+			toS += padding+padding+goodRel+"**\n";
+		}
+
+		for (String badRel: incorrectRelatives(goldStandardRelatives)) {
+			toS += padding+padding+badRel+"\n";
+		}
+
+		toS += padding+"Gold Standard relatives  (** = found):\n";
+		for (String gsRel: goldStandardRelatives) {
+			toS += padding+padding+gsRel;
+			if (goodRels.contains(gsRel)) {
+				toS += "**";
+			}
+			toS += "\n";
+		}
+
+		return toS;
+	}
+
+	public double recall(String[] gsRels) {
+		double rec =  0.0;
+		if (gsRels != null && gsRels.length > 0) {
+			rec = 1.0 * correctRelatives(gsRels).size() / gsRels.length;
+		}
+		return rec;
+	}
+
+	public double precision(String[] gsRels) {
+		double prec = 0.0;
+		String[] produced = relsProduced;
+		if (produced != null && produced.length > 0) {
+			prec = 1.0 * correctRelatives(gsRels).size() / produced.length;
+		}
+		return prec;
+	}
+}
---
+++ iutools-core/src/test/java/org/iutools/morphrelatives/WordOutcomeTest.java
@@ -0,0 +1,106 @@
+package org.iutools.morphrelatives;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class WordOutcomeTest {
+
+	WordOutcome outcome = null;
+	String[] goldStandardRelatives = new String[] {
+		"takujumaguvit", "takujumajuq", "takujumajut", "takugumajunga",
+		"takugumavugut", "takujumagatta", "takujumavunga"
+	};
+
+	@BeforeEach
+	public void setUp() {
+		String word = "takujumaguvit";
+		String[] relativesProduced = new String[] {
+			"takugumajunga",  "takugumavugut", "takujumagatta", "takujumavunga",
+			// This last one should be considered incorrect
+			"takujumajuit"
+		};
+		outcome = new WordOutcome(word, relativesProduced);
+	}
+
+	////////////////////////////////
+	// DOCUMENTATION TESTS
+	////////////////////////////////
+
+	@Test
+	public void test__WordOutcome__Synopsis() {
+		// This class represented the outcome (expected or actual) of running the
+		// MorphRelativesFinder on a word
+		//
+		String word = "takujumaguvit";
+		String[] relativesProduced = new String[] {
+			"takugumajunga",  "takugumavugut", "takujumagatta", "takujumavunga",
+			"takujumajut"
+		};
+		WordOutcome outcome = new WordOutcome(word, relativesProduced);
+
+		// You can compare a word outcome to what should have been produced
+		// by a "perfect" algorigthm.
+		//
+		String[] goldStandardRelatives = new String[] {
+			"takujumaguvit", "takujumajuq", "takujumajut", "takugumajunga",
+			"takugumavugut", "takujumagatta", "takujumavunga"
+		};
+		List<String> correctRels = outcome.correctRelatives(goldStandardRelatives);
+		List<String> incorrectRels = outcome.incorrectRelatives(goldStandardRelatives);
+
+		// You can also get the precision and recall for that word
+		double prec = outcome.precision(goldStandardRelatives);
+		double recall = outcome.recall(goldStandardRelatives);
+
+		// You can also get a text summary of the outcome
+		String outcomeSummary = outcome.diffsWithGoldStandard(goldStandardRelatives);
+	}
+
+	////////////////////////////////
+	// VERIFICATION TESTS
+	////////////////////////////////
+
+	@Test
+	public void test__WordOutcome__HappyPath() throws Exception {
+		new AssertWordOutcome(outcome)
+			.precisionIs(goldStandardRelatives, 0.8)
+			.recallIs(goldStandardRelatives, 0.57)
+			.relativesProducedAre(
+				"takugumajunga",
+				"takugumavugut",
+				"takujumagatta",
+				"takujumavunga",
+				"takujumajuit"
+			)
+			.correctRelativesAre(
+				goldStandardRelatives,
+				"takugumajunga",
+				"takugumavugut",
+				"takujumagatta",
+				"takujumavunga"
+			)
+			.incorrectRelativesAre(goldStandardRelatives,"takujumajuit")
+			.prettyPrintIs(goldStandardRelatives,
+				"  Word: takujumaguvit\n" +
+				"  Precision: 0.8\n" +
+				"  Recal: 0.5714285714285714\n" +
+				"  Relatives produced (** = correct): \n" +
+				"    takugumajunga**\n" +
+				"    takugumavugut**\n" +
+				"    takujumagatta**\n" +
+				"    takujumavunga**\n" +
+				"    takujumajuit\n" +
+				"  Gold Standard relatives  (** = found):\n" +
+				"    takujumaguvit\n" +
+				"    takujumajuq\n" +
+				"    takujumajut\n" +
+				"    takugumajunga**\n" +
+				"    takugumavugut**\n" +
+				"    takujumagatta**\n" +
+				"    takujumavunga**\n"
+				)
+			;
+	}
+}
