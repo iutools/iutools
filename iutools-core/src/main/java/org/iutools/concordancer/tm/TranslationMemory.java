54f1c285861056d88e51384ffa79dea2f16a26ab
desiletsa <alain.desilets@nrc-cnrc.gc.ca>
2021-08-09 08:22:02 -0400

    [ENH] Accelerate IUWordDict

--- iutools-core/src/main/java/org/iutools/concordancer/tm/TranslationMemory.java
+++ iutools-core/src/main/java/org/iutools/concordancer/tm/TranslationMemory.java
@@ -5,12 +5,14 @@ import ca.nrc.dtrc.elasticsearch.ElasticSearchException;
 import ca.nrc.dtrc.elasticsearch.SearchResults;
 import ca.nrc.dtrc.elasticsearch.StreamlinedClient;
 import ca.nrc.ui.commandline.UserIO;
+import org.apache.commons.collections4.iterators.IteratorChain;
 import org.apache.log4j.Logger;
 import org.iutools.concordancer.Alignment_ES;
 import org.iutools.script.TransCoder;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 
@@ -85,8 +87,47 @@ public class TranslationMemory {
 
 	public List<Alignment_ES> search(String sourceLang, String sourceExpr,
 		String... targetLangs) throws TranslationMemoryException {
-
 		List<Alignment_ES> alignments = new ArrayList<Alignment_ES>();
+		try {
+			Iterator<Alignment_ES> iter = searchIter(sourceLang,sourceExpr, targetLangs);
+			while (iter.hasNext()) {
+				alignments.add(iter.next());
+			}
+//			String[] sourceExprVariants = new String[]{sourceExpr};
+//			if (sourceLang.equals("iu")) {
+//				// For iu, try the search with both scripts.
+//				// Some of the TMs use roman while others use syllabic
+//				sourceExprVariants = new String[]{
+//				TransCoder.ensureScript(TransCoder.Script.SYLLABIC, sourceExpr),
+//				TransCoder.ensureScript(TransCoder.Script.ROMAN, sourceExpr),
+//				};
+//			}
+//
+//			for (String expr: sourceExprVariants) {
+//				String freeformQuery = "sentences."+sourceLang+":\""+expr+"\"";
+//				SearchResults<Alignment_ES> searchResult = null;
+//				try {
+//					searchResult = esClient()
+//						.search(freeformQuery, ES_ALIGNMENT_TYPE, new Alignment_ES());
+//					for (
+//						DocIterator<Alignment_ES> it = searchResult.docIterator(); it.hasNext(); ) {
+//						Alignment_ES aHit = it.next();
+//						alignments.add(aHit);
+//					}
+//				} catch (ElasticSearchException e) {
+//					throw new TranslationMemoryException(e);
+//				}
+//			}
+		} catch (Exception e) {
+			throw new TranslationMemoryException(e);
+		}
+
+		return alignments;
+	}
+
+	private Iterator<Alignment_ES> searchIter(String sourceLang, String sourceExpr, String[] targetLangs) throws TranslationMemoryException {
+		List<Iterator<Alignment_ES>> iterators =
+			new ArrayList<Iterator<Alignment_ES>>();
 		try {
 			String[] sourceExprVariants = new String[]{sourceExpr};
 			if (sourceLang.equals("iu")) {
@@ -104,11 +145,7 @@ public class TranslationMemory {
 				try {
 					searchResult = esClient()
 						.search(freeformQuery, ES_ALIGNMENT_TYPE, new Alignment_ES());
-					for (
-						DocIterator<Alignment_ES> it = searchResult.docIterator(); it.hasNext(); ) {
-						Alignment_ES aHit = it.next();
-						alignments.add(aHit);
-					}
+					iterators.add(searchResult.docIterator());
 				} catch (ElasticSearchException e) {
 					throw new TranslationMemoryException(e);
 				}
@@ -117,7 +154,15 @@ public class TranslationMemory {
 			throw new TranslationMemoryException(e);
 		}
 
-		return alignments;
+		Iterator<Alignment_ES> iterator = null;
+		if (iterators.size() == 1) {
+			iterator = iterators.get(0);
+		} else {
+			iterator =
+				new IteratorChain<Alignment_ES>(iterators.get(0), iterators.get(1));
+		}
+
+		return iterator;
 	}
 
 	public void deleteIndex() throws TranslationMemoryException {
